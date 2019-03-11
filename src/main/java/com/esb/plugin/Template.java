9fd2edff391fc5552516b36adc2629de6f02da0e
lorenzo <lorenzo@lorenzo.info.com>
2019-03-11 18:12:29 +0100

    POMTemplating

---
+++ src/main/java/com/esb/plugin/Template.java
@@ -0,0 +1,11 @@
+package com.esb.plugin;
+
+public class Template {
+
+    public class Internal {
+
+        public static final String MAVEN_PROJECT = "Maven Project";
+        public static final String MAVEN_MODULE = "Maven Module";
+
+    }
+}
--- src/main/java/com/esb/plugin/module/ESBModuleBuilder.java
+++ src/main/java/com/esb/plugin/module/ESBModuleBuilder.java
@@ -2,27 +2,30 @@ package com.esb.plugin.module;
 
 import com.esb.plugin.ESBIcons;
 import com.esb.plugin.module.wizard.step.ConfigureRuntimeStep;
-import com.esb.plugin.module.wizard.step.EmptyStep;
-import com.esb.plugin.module.wizard.step.MavenConfigStep;
-import com.esb.plugin.templating.POMTemplate;
-import com.intellij.ide.util.projectWizard.ModuleBuilder;
+import com.intellij.ide.util.projectWizard.ModuleBuilderListener;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.JavaModuleType;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.project.DumbAwareRunnable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
-import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
 import com.intellij.openapi.util.Disposer;
-import freemarker.template.TemplateException;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.maven.model.MavenId;
+import org.jetbrains.idea.maven.utils.MavenUtil;
+import org.jetbrains.idea.maven.wizards.MavenModuleBuilder;
 
 import javax.swing.*;
-import java.io.IOException;
+import java.io.File;
 
-public class ESBModuleBuilder extends ModuleBuilder {
+public class ESBModuleBuilder extends MavenModuleBuilder {
 
     private String groupId;
     private String version;
@@ -31,37 +34,47 @@ public class ESBModuleBuilder extends ModuleBuilder {
 
     private String runtimeHome;
 
+    public ESBModuleBuilder() {
+        setProjectId(new MavenId("com.esb.module", "sample-module", "1.0.0-SNAPSHOT"));
+    }
+
 
     @Override
     public void setupRootModel(@NotNull ModifiableRootModel rootModel) {
+        super.setupRootModel(rootModel);
+
+        addListener(new ModuleBuilderListener() {
+            @Override
+            public void moduleCreated(@NotNull Module module) {
+                // After module created
+            }
+        });
+
         final Project project = rootModel.getProject();
-        if (myJdk != null){
-            rootModel.setSdk(myJdk);
-        } else {
-            rootModel.inheritSdk();
-        }
+        final VirtualFile root = createAndGetContentEntry();
+
+        rootModel.addContentEntry(root);
+
+        final MavenId parentId = getParentProject() != null ? getParentProject().getMavenId() : null;
+        final MavenId projectId = getProjectId();
+
+        MavenUtil.runWhenInitialized(project, (DumbAwareRunnable) () ->
+                new ESBMavenProjectBuilderHelper().configure(project, projectId, parentId, root));
     }
 
-    @Override
-    public Project createProject(String name, String path) {
-
-        Project project = super.createProject(name, path);
-
-        POMTemplate template = new POMTemplate();
-        try {
-            template.create(groupId, version, artifactId, javaVersion, project.getBasePath());
-        } catch (IOException | TemplateException e) {
-            e.printStackTrace();
-        }
-
-        return project;
+    private VirtualFile createAndGetContentEntry() {
+        String path = FileUtil.toSystemIndependentName(this.getContentEntryPath());
+        (new File(path)).mkdirs();
+        return LocalFileSystem.getInstance().refreshAndFindFileByPath(path);
     }
 
+
+    /**
     @Override
     public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext, @NotNull ModulesProvider modulesProvider) {
         return new ModuleWizardStep[] {
                 new MavenConfigStep(wizardContext, this) };
-    }
+    }*/
 
     public void setGroupId(String groupId) {
         this.groupId = groupId;
