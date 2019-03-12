d521eed60de422e897a8c8efaadac39e993c3347
lorenzo <lorenzo@lorenzo.info.com>
2019-03-12 17:39:44 +0100

    Feature #22: IntelliJ ESB Plugin create Runtime Start config

--- src/main/java/com/esb/plugin/runconfig/module/ESBModuleRunConfigurationFactory.java
+++ src/main/java/com/esb/plugin/runconfig/module/ESBModuleRunConfigurationFactory.java
@@ -1,10 +1,12 @@
 package com.esb.plugin.runconfig.module;
 
 import com.esb.plugin.runconfig.runtime.ESBRuntimeRunConfiguration;
+import com.intellij.execution.BeforeRunTask;
 import com.intellij.execution.configurations.ConfigurationFactory;
 import com.intellij.execution.configurations.ConfigurationType;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
 import org.jetbrains.annotations.NotNull;
 
 public class ESBModuleRunConfigurationFactory extends ConfigurationFactory {
@@ -26,5 +28,10 @@ public class ESBModuleRunConfigurationFactory extends ConfigurationFactory {
     @Override
     public String getName() {
         return FACTORY_NAME;
+    }
+
+    @Override
+    public void configureBeforeRunTaskDefaults(Key<? extends BeforeRunTask> providerID, BeforeRunTask task) {
+        super.configureBeforeRunTaskDefaults(providerID, task);
     }
 }
