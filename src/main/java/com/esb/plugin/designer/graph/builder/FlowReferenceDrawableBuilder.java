9a82d29f31ab511297564aacfd1563a96377cb4e
lorenzo <lorenzo@lorenzo.info.com>
2019-03-28 10:29:33 +0100

    Feature #34: Flow Designer: skeleton + palette for components.

--- src/main/java/com/esb/plugin/designer/graph/builder/FlowReferenceDrawableBuilder.java
+++ src/main/java/com/esb/plugin/designer/graph/builder/FlowReferenceDrawableBuilder.java
@@ -1,13 +1,25 @@
 package com.esb.plugin.designer.graph.builder;
 
+import com.esb.internal.commons.JsonParser;
+import com.esb.plugin.designer.editor.component.Component;
 import com.esb.plugin.designer.graph.FlowGraph;
 import com.esb.plugin.designer.graph.drawable.Drawable;
+import com.esb.plugin.designer.graph.drawable.FlowReferenceDrawable;
 import org.json.JSONObject;
 
 public class FlowReferenceDrawableBuilder implements Builder {
 
     @Override
     public Drawable build(Drawable parent, JSONObject componentDefinition, FlowGraph graph) {
-        return null;
+
+        String name = JsonParser.Implementor.name(componentDefinition);
+
+        Component component = new Component(name);
+
+        FlowReferenceDrawable flowReferenceDrawable = new FlowReferenceDrawable(component);
+
+        graph.add(parent, flowReferenceDrawable);
+
+        return flowReferenceDrawable;
     }
 }
--- src/main/java/com/esb/plugin/designer/graph/drawable/FlowReferenceDrawable.java
+++ src/main/java/com/esb/plugin/designer/graph/drawable/FlowReferenceDrawable.java
@@ -1,4 +1,9 @@
 package com.esb.plugin.designer.graph.drawable;
 
-public class FlowReferenceDrawable {
+import com.esb.plugin.designer.editor.component.Component;
+
+public class FlowReferenceDrawable extends AbstractDrawable {
+    public FlowReferenceDrawable(Component component) {
+        super(component);
+    }
 }
