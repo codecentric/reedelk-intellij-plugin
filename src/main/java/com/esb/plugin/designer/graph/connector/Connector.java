8c9066fad2f06005d383184df81600bd0c42b2f8
lorenzo <lorenzo@lorenzo.info.com>
2019-04-13 11:05:56 +0200

    Bugfix + Tests

--- src/main/java/com/esb/plugin/designer/graph/connector/Connector.java
+++ src/main/java/com/esb/plugin/designer/graph/connector/Connector.java
@@ -5,15 +5,16 @@ import com.esb.plugin.designer.graph.drawable.ScopedDrawable;
 
 public interface Connector {
 
-    void addSuccessor(Drawable successor);
-
-    void addPredecessor(Drawable predecessor);
-
     void add();
 
     void root();
 
     void addToScope(ScopedDrawable scope);
 
+    void addSuccessor(Drawable successor);
+
+    void addPredecessor(Drawable predecessor);
+
     void addPredecessor(ScopedDrawable predecessor, int index);
+
 }
