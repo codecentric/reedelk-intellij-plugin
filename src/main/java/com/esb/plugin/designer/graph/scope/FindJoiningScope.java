319096e4d4f5d2cfbaa2e5969ca7bca9776082a5
lorenzo <lorenzo@lorenzo.info.com>
2019-04-23 10:20:31 +0200

    Tests + Refactoring

--- src/main/java/com/esb/plugin/designer/graph/scope/FindJoiningScope.java
+++ src/main/java/com/esb/plugin/designer/graph/scope/FindJoiningScope.java
@@ -9,19 +9,22 @@ import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
-// TODO: Test this method
 public class FindJoiningScope {
 
-    public static Optional<ScopedDrawable> of(FlowGraph graph, Drawable drawable) {
+    public static Optional<ScopedDrawable> of(FlowGraph graph, Drawable target) {
         List<ScopedDrawable> scopedDrawables = ListAllScopes.of(graph);
-        List<ScopedDrawable> scopes = scopedDrawables.stream()
+        List<ScopedDrawable> scopesWithTargetAsFirstOutsideNode = scopedDrawables.stream()
                 .filter(scopedDrawable -> {
                     Optional<Drawable> firstNode = FindFirstNodeOutsideScope.of(graph, scopedDrawable);
-                    return firstNode.filter(drawable1 -> drawable1 == drawable).isPresent();
+                    return firstNode.filter(firstNodeOutsideScope -> firstNodeOutsideScope == target).isPresent();
                 }).collect(toList());
 
-        // TODO: we need to select the outer most amongst all the scopes.
-        // The outermost is the one with the lowest X
+        return filterOutermostScope(scopesWithTargetAsFirstOutsideNode);
+    }
+
+    // We must to return the outermost amongst all the scopes.
+    // The outermost is the one with the lowest X coordinate.
+    private static Optional<ScopedDrawable> filterOutermostScope(List<ScopedDrawable> scopes) {
         ScopedDrawable outermost = null;
         for (ScopedDrawable scope : scopes) {
             if (outermost == null) {
@@ -32,7 +35,6 @@ public class FindJoiningScope {
                 outermost = scope;
             }
         }
-
         return Optional.ofNullable(outermost);
     }
 }
