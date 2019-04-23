ec7a02c6deb144040ce67e20be926775dcc68b28
lorenzo <lorenzo@lorenzo.info.com>
2019-04-23 10:58:21 +0200

    Tests + Refactoring

---
+++ src/main/java/com/esb/plugin/designer/graph/drawable/AbstractScopedDrawable.java
@@ -0,0 +1,106 @@
+package com.esb.plugin.designer.graph.drawable;
+
+import com.esb.plugin.designer.editor.component.Component;
+import com.esb.plugin.designer.graph.FlowGraph;
+import com.esb.plugin.designer.graph.drawable.decorators.Arrow;
+import com.esb.plugin.designer.graph.drawable.decorators.ScopeBoundariesDrawable;
+import com.esb.plugin.designer.graph.drawable.decorators.VerticalDivider;
+import com.esb.plugin.designer.graph.scope.FindFirstNodeOutsideScope;
+import com.esb.plugin.designer.graph.scope.IsLastScopeBeforeNode;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.util.List;
+import java.util.*;
+
+abstract class AbstractScopedDrawable extends AbstractDrawable implements ScopedDrawable {
+
+    private final Drawable verticalDivider;
+    private final ScopeBoundariesDrawable scopeBoundariesDrawable;
+
+    private Set<Drawable> scope = new HashSet<>();
+
+    AbstractScopedDrawable(Component component) {
+        super(component);
+        this.verticalDivider = new VerticalDivider(this);
+        this.scopeBoundariesDrawable = new ScopeBoundariesDrawable(this);
+    }
+
+    @Override
+    public boolean scopeContains(Drawable drawable) {
+        return scope.contains(drawable);
+    }
+
+    @Override
+    public ScopeBoundaries getScopeBoundaries(FlowGraph graph, Graphics2D graphics) {
+        return scopeBoundariesDrawable.getBoundaries(graph, graphics);
+    }
+
+    @Override
+    public void draw(FlowGraph graph, Graphics2D graphics, ImageObserver observer) {
+        super.draw(graph, graphics, observer);
+        verticalDivider.draw(graph, graphics, observer);
+        scopeBoundariesDrawable.draw(graph, graphics, observer);
+    }
+
+    @Override
+    protected void drawConnections(FlowGraph graph, Graphics2D graphics, ImageObserver observer) {
+        // Draw arrows -> perpendicular to the vertical bar.
+        int halfWidth = Math.floorDiv(width(graphics), 2);
+        int verticalX = x() + halfWidth - 6;
+
+        List<Drawable> successors = graph.successors(this);
+        for (Drawable successor : successors) {
+
+            Point targetBaryCenter = successor.getBarycenter(graphics);
+            Point sourceBaryCenter = new Point(verticalX, targetBaryCenter.y);
+            Point target = getTarget(graphics, successor);
+
+            Arrow arrow = new Arrow(sourceBaryCenter, target);
+            arrow.draw(graphics);
+        }
+
+        drawEndOfScopeArrow(graph, graphics);
+    }
+
+    @Override
+    public void addToScope(Drawable drawable) {
+        this.scope.add(drawable);
+    }
+
+    @Override
+    public void removeFromScope(Drawable drawable) {
+        this.scope.remove(drawable);
+    }
+
+    @Override
+    public Collection<Drawable> getScope() {
+        return Collections.unmodifiableSet(scope);
+    }
+
+    // We also need to draw a connection between the end of scope to the next successor.
+    // We draw this arrow only if the last drawables of this scope connect
+    // arrows in the next scope
+    private void drawEndOfScopeArrow(FlowGraph graph, Graphics2D graphics) {
+        FindFirstNodeOutsideScope.of(graph, this).ifPresent(firstNodeOutsideScope -> {
+            if (IsLastScopeBeforeNode.of(graph, AbstractScopedDrawable.this, firstNodeOutsideScope)) {
+                ScopeBoundaries boundaries = scopeBoundariesDrawable.getBoundaries(graph, graphics);
+
+                Point barycenter = firstNodeOutsideScope.getBarycenter(graphics);
+                Point source = new Point(boundaries.getX() + boundaries.getWidth(), barycenter.y);
+                Point target = getTarget(graphics, firstNodeOutsideScope);
+
+                Arrow arrow = new Arrow(source, target);
+                arrow.draw(graphics);
+            }
+        });
+    }
+
+    private Point getTarget(Graphics2D graphics, Drawable drawable) {
+        Point barycenter = drawable.getBarycenter(graphics);
+        return new Point(
+                barycenter.x - Math.floorDiv(drawable.width(graphics), 2) + 15,
+                barycenter.y);
+    }
+
+}
--- src/main/java/com/esb/plugin/designer/graph/drawable/ForkJoinDrawable.java
+++ src/main/java/com/esb/plugin/designer/graph/drawable/ForkJoinDrawable.java
@@ -1,43 +1,10 @@
 package com.esb.plugin.designer.graph.drawable;
 
 import com.esb.plugin.designer.editor.component.Component;
-import com.esb.plugin.designer.graph.FlowGraph;
 
-import java.awt.*;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
-public class ForkJoinDrawable extends AbstractDrawable implements ScopedDrawable {
-
-    private Set<Drawable> scope = new HashSet<>();
+public class ForkJoinDrawable extends AbstractScopedDrawable {
 
     public ForkJoinDrawable(Component component) {
         super(component);
     }
-
-    public void addToScope(Drawable drawable) {
-        this.scope.add(drawable);
-    }
-
-    public void removeFromScope(Drawable drawable) {
-        this.scope.remove(drawable);
-    }
-
-    public Collection<Drawable> getScope() {
-        return Collections.unmodifiableSet(scope);
-    }
-
-    @Override
-    public boolean scopeContains(Drawable drawable) {
-        return scope.contains(drawable);
-    }
-
-    @Override
-    public ScopeBoundaries getScopeBoundaries(FlowGraph graph, Graphics2D graphics) {
-        return null;
-    }
-
-
 }
