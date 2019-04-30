8b47c5bd94caf8c269192052879609cf00a6e165
lorenzo <lorenzo@lorenzo.info.com>
2019-04-30 13:51:37 +0200

    Code

--- src/main/java/com/esb/plugin/component/choice/ChoicePropertiesRenderer.java
+++ src/main/java/com/esb/plugin/component/choice/ChoicePropertiesRenderer.java
@@ -6,6 +6,13 @@ import com.esb.plugin.graph.FlowGraph;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.components.JBPanel;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.table.JBTable;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
 
 public class ChoicePropertiesRenderer extends AbstractPropertyRenderer {
 
@@ -15,6 +22,45 @@ public class ChoicePropertiesRenderer extends AbstractPropertyRenderer {
 
     @Override
     public void render(JBPanel panel, Component component) {
+        TableModel model = new MyTableModel();
 
+        final JBTable table = new JBTable(model);
+        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+        table.setFillsViewportHeight(true);
+
+        //Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JBScrollPane(table);
+
+        //Add the scroll pane to this panel.
+        panel.add(scrollPane);
+    }
+
+    class MyTableModel extends AbstractTableModel {
+        String[] columnNames = {"Condition", "Route"};
+        Object[][] data = {
+                {"payload.name == 'Mark'", "route1"},
+                {"payload.name == 'Anton'", "route2"},
+        };
+
+
+        @Override
+        public int getRowCount() {
+            return data.length;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columnNames.length;
+        }
+
+        @Override
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return data[rowIndex][columnIndex];
+        }
     }
 }
