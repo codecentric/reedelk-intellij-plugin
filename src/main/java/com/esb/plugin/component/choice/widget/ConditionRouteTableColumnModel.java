a43003bcd1f93ced18879f7c65a7b87515ff7e1d
lorenzo <lorenzo@lorenzo.info.com>
2019-05-03 16:11:04 +0200

    Code

--- src/main/java/com/esb/plugin/component/choice/widget/ConditionRouteTableColumnModel.java
+++ src/main/java/com/esb/plugin/component/choice/widget/ConditionRouteTableColumnModel.java
@@ -1,8 +1,13 @@
 package com.esb.plugin.component.choice.widget;
 
+import com.esb.plugin.graph.node.GraphNode;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.TableColumn;
+
+import static com.esb.plugin.designer.canvas.drawables.ComponentDescription.DESCRIPTION_PROPERTY_NAME;
 
 class ConditionRouteTableColumnModel extends DefaultTableColumnModel {
 
@@ -16,7 +21,18 @@ class ConditionRouteTableColumnModel extends DefaultTableColumnModel {
         TableColumn routeColumn = new TableColumn(1);
         routeColumn.setHeaderValue(ConditionRouteColumns.COLUMN_NAMES[1]);
         routeColumn.setCellEditor(new DefaultCellEditor(routesComboBox));
+        routeColumn.setCellRenderer(new RoutesCellRenderer());
+
         addColumn(routeColumn);
+    }
+
+    class RoutesCellRenderer extends DefaultTableCellRenderer {
+        @Override
+        protected void setValue(Object value) {
+            GraphNode node = (GraphNode) value;
+            String description = (String) node.component().getData(DESCRIPTION_PROPERTY_NAME);
+            setText(description);
+        }
     }
 
 }
