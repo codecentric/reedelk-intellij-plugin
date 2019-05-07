828f2a1e566dca566d9e9347f6edefd0b83075f0
lorenzo <lorenzo@lorenzo.info.com>
2019-05-07 16:14:15 +0200

    Code

--- src/test/java/com/esb/plugin/component/choice/ChoiceSerializerTest.java
+++ src/test/java/com/esb/plugin/component/choice/ChoiceSerializerTest.java
@@ -13,6 +13,9 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.esb.plugin.component.choice.ChoiceNode.DATA_CONDITION_ROUTE_PAIRS;
+import static com.esb.plugin.component.choice.ChoiceNode.DEFAULT_CONDITION_NAME;
+
 public class ChoiceSerializerTest extends AbstractGraphTest {
 
     private ChoiceSerializer serializer;
@@ -29,22 +32,36 @@ public class ChoiceSerializerTest extends AbstractGraphTest {
         FlowGraph graph = new FlowGraphImpl();
         graph.root(root);
         graph.add(root, choice1);
+
         graph.add(choice1, n1);
-        graph.add(choice1, n2);
-        graph.add(n1, n3);
-        graph.add(n2, n3);
+        graph.add(n1, n2);
+
+        graph.add(choice1, n3);
+        graph.add(n3, n4);
+
+        graph.add(choice1, n5);
+        graph.add(n5, n6);
+
+        graph.add(n2, n7);
+        graph.add(n4, n7);
+        graph.add(n6, n7);
 
         choice1.addToScope(n1);
         choice1.addToScope(n2);
+        choice1.addToScope(n3);
+        choice1.addToScope(n4);
+        choice1.addToScope(n5);
+        choice1.addToScope(n6);
 
         ComponentData component = choice1.component();
         List<ChoiceConditionRoutePair> choiceRoute = new ArrayList<>();
-        choiceRoute.add(new ChoiceConditionRoutePair(ChoiceNode.DEFAULT_CONDITION_NAME, n1));
-        choiceRoute.add(new ChoiceConditionRoutePair("payload != null", n2));
-        component.set(ChoiceNode.DATA_CONDITION_ROUTE_PAIRS, choiceRoute);
+        choiceRoute.add(new ChoiceConditionRoutePair(DEFAULT_CONDITION_NAME, n1));
+        choiceRoute.add(new ChoiceConditionRoutePair("payload == 'Mark'", n3));
+        choiceRoute.add(new ChoiceConditionRoutePair("payload == 'John'", n5));
+        component.set(DATA_CONDITION_ROUTE_PAIRS, choiceRoute);
 
         // When
-        JSONObject serializedObject = serializer.serialize(graph, choice1, n3);
+        JSONObject serializedObject = serializer.serialize(graph, choice1, n7);
 
         // Then
         String actualJson = serializedObject.toString(2);
