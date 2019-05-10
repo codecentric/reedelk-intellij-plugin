5bdc474e509552c1f8398b40cd3e1a66b574f7b2
lorenzo <lorenzo@lorenzo.info.com>
2019-05-10 13:20:15 +0200

    Code

--- src/test/java/com/esb/plugin/component/choice/ChoiceSerializerTest.java
+++ src/test/java/com/esb/plugin/component/choice/ChoiceSerializerTest.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.esb.plugin.component.choice.ChoiceNode.DATA_CONDITION_ROUTE_PAIRS;
 import static com.esb.plugin.component.choice.ChoiceNode.DEFAULT_CONDITION_NAME;
 import static com.esb.plugin.fixture.Json.Choice;
+import static java.util.Arrays.asList;
 
 public class ChoiceSerializerTest extends AbstractGraphTest {
 
@@ -27,7 +27,7 @@ public class ChoiceSerializerTest extends AbstractGraphTest {
     }
 
     @Test
-    void shouldCorrectlySerializeChoiceWithNodeOutsideScope() {
+    void shouldCorrectlySerializeChoiceNode() {
         // Given
         FlowGraph graph = new FlowGraphImpl();
         graph.root(root);
@@ -52,56 +52,16 @@ public class ChoiceSerializerTest extends AbstractGraphTest {
         choiceNode1.addToScope(componentNode5);
         choiceNode1.addToScope(componentNode6);
 
-        List<ChoiceConditionRoutePair> choiceRoute = new ArrayList<>();
-        choiceRoute.add(new ChoiceConditionRoutePair("1 == 1", componentNode3));
-        choiceRoute.add(new ChoiceConditionRoutePair("1 != 0", componentNode2));
-        choiceRoute.add(new ChoiceConditionRoutePair(DEFAULT_CONDITION_NAME, componentNode5));
+        List<ChoiceConditionRoutePair> choiceRoute = asList(
+                new ChoiceConditionRoutePair("1 == 1", componentNode3),
+                new ChoiceConditionRoutePair("1 != 0", componentNode2),
+                new ChoiceConditionRoutePair(DEFAULT_CONDITION_NAME, componentNode5));
 
         ComponentData component = choiceNode1.componentData();
         component.set(DATA_CONDITION_ROUTE_PAIRS, choiceRoute);
 
         // When
         JSONObject serializedObject = serializer.serialize(graph, choiceNode1, componentNode7);
-
-        // Then
-        String actualJson = serializedObject.toString(2);
-        String expectedJson = Choice.Sample.json();
-        JSONAssert.assertEquals(expectedJson, actualJson, true);
-    }
-
-    @Test
-    void shouldCorrectlySerializeChoiceWithoutNodeOutsideScope() {
-        // Given
-        FlowGraph graph = new FlowGraphImpl();
-        graph.root(root);
-        graph.add(root, choiceNode1);
-
-        graph.add(choiceNode1, componentNode3);
-        graph.add(componentNode3, componentNode1);
-
-        graph.add(choiceNode1, componentNode2);
-        graph.add(componentNode2, componentNode4);
-
-        graph.add(choiceNode1, componentNode5);
-        graph.add(componentNode5, componentNode6);
-
-        choiceNode1.addToScope(componentNode1);
-        choiceNode1.addToScope(componentNode2);
-        choiceNode1.addToScope(componentNode3);
-        choiceNode1.addToScope(componentNode4);
-        choiceNode1.addToScope(componentNode5);
-        choiceNode1.addToScope(componentNode6);
-
-        List<ChoiceConditionRoutePair> choiceRoute = new ArrayList<>();
-        choiceRoute.add(new ChoiceConditionRoutePair("1 == 1", componentNode3));
-        choiceRoute.add(new ChoiceConditionRoutePair("1 != 0", componentNode2));
-        choiceRoute.add(new ChoiceConditionRoutePair(DEFAULT_CONDITION_NAME, componentNode5));
-
-        ComponentData component = choiceNode1.componentData();
-        component.set(DATA_CONDITION_ROUTE_PAIRS, choiceRoute);
-
-        // When
-        JSONObject serializedObject = serializer.serialize(graph, choiceNode1, null);
 
         // Then
         String actualJson = serializedObject.toString(2);
