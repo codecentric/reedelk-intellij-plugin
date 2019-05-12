61b36ff766bd77d9b21bd9f2f1aed5663cc04dce
lorenzo <lorenzo@lorenzo.info.com>
2019-05-12 11:45:08 +0200

    Code

--- src/main/java/com/esb/plugin/graph/serializer/AbstractNodeSerializer.java
+++ src/main/java/com/esb/plugin/graph/serializer/AbstractNodeSerializer.java
@@ -10,28 +10,29 @@ import java.util.List;
 import static com.esb.internal.commons.Preconditions.checkState;
 
 public abstract class AbstractNodeSerializer implements Serializer {
+
     @Override
     public void serialize(FlowGraph graph, JSONArray sequence, GraphNode node, GraphNode stop) {
+
         JSONObject serializedObject = serializeNode(graph, node, stop);
 
         sequence.put(serializedObject);
 
         List<GraphNode> successors = graph.successors(node);
+
         checkState(successors.size() <= 1,
                 "Only scoped nodes might have more than one successors");
 
         if (!successors.isEmpty()) {
-            GraphNode successorNode = successors.get(0);
-            if (stop instanceof GraphSerializer.UntilNoSuccessors) {
+
+            GraphNode successor = successors.get(0);
+
+            if (stop != successor) {
+
                 GraphSerializerFactory.get()
-                        .node(successorNode)
+                        .node(successor)
                         .build()
-                        .serialize(graph, sequence, successorNode, stop);
-            } else if (stop != successorNode) {
-                GraphSerializerFactory.get()
-                        .node(successorNode)
-                        .build()
-                        .serialize(graph, sequence, successorNode, stop);
+                        .serialize(graph, sequence, successor, stop);
             }
         }
     }
