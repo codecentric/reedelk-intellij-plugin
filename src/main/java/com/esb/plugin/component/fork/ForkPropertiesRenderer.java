d8ad80588770cb4ce9ff75c2880ca6d1b7145bae
lorenzo <lorenzo@lorenzo.info.com>
2019-05-18 18:28:11 +0200

    Code

--- src/main/java/com/esb/plugin/component/fork/ForkPropertiesRenderer.java
+++ src/main/java/com/esb/plugin/component/fork/ForkPropertiesRenderer.java
@@ -1,35 +1,12 @@
 package com.esb.plugin.component.fork;
 
-import com.esb.plugin.component.ComponentData;
-import com.esb.plugin.component.ComponentPropertyDescriptor;
-import com.esb.plugin.designer.properties.renderer.node.AbstractNodePropertiesRenderer;
-import com.esb.plugin.designer.properties.renderer.type.TypeRendererFactory;
-import com.esb.plugin.designer.properties.widget.DefaultPropertiesPanel;
+import com.esb.plugin.component.generic.GenericComponentPropertiesRenderer;
 import com.esb.plugin.graph.GraphSnapshot;
-import com.esb.plugin.graph.node.GraphNode;
-import com.intellij.ui.components.JBPanel;
 
-import java.util.List;
-
-public class ForkPropertiesRenderer extends AbstractNodePropertiesRenderer {
+public class ForkPropertiesRenderer extends GenericComponentPropertiesRenderer {
 
     public ForkPropertiesRenderer(GraphSnapshot snapshot) {
         super(snapshot);
-    }
-
-    @Override
-    public JBPanel render(GraphNode node) {
-        ComponentData componentData = node.componentData();
-
-        DefaultPropertiesPanel panel = new DefaultPropertiesPanel();
-
-        List<ComponentPropertyDescriptor> componentProperties = componentData.getComponentPropertyDescriptors();
-        componentProperties.forEach(propertyDescriptor ->
-                TypeRendererFactory.get()
-                        .from(propertyDescriptor.getPropertyType())
-                        .render(propertyDescriptor, componentData, snapshot));
-
-        return panel;
     }
 
 }
