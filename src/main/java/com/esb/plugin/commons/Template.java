31d620937edb363f975da40f8ccad9e44f7d2d8d
lorenzo <lorenzo@lorenzo.info.com>
2019-05-23 09:38:21 +0200

    Code

--- src/main/java/com/esb/plugin/commons/Template.java
+++ src/main/java/com/esb/plugin/commons/Template.java
@@ -1,11 +1,8 @@
 package com.esb.plugin.commons;
 
 public class Template {
-
     public class Internal {
-
         public static final String MAVEN_PROJECT = "Maven Project";
         public static final String MAVEN_MODULE = "Maven Module";
-
     }
 }
