ea314e3a810506f80372a8f248581a19139a689f
lorenzo <lorenzo@lorenzo.info.com>
2019-05-28 09:50:49 +0200

    Code

--- src/main/java/com/esb/plugin/graph/action/strategy/PrecedingScopedNode.java
+++ src/main/java/com/esb/plugin/graph/action/strategy/PrecedingScopedNode.java
@@ -9,7 +9,6 @@ import com.esb.plugin.graph.utils.FindFirstNodeOutsideScope;
 
 import java.awt.*;
 import java.util.List;
-import java.util.Optional;
 
 import static com.google.common.base.Preconditions.checkState;
 
@@ -25,72 +24,68 @@ public class PrecedingScopedNode extends AbstractAddStrategy {
 
     // It is the only type of node with potentially many successors.
     @Override
-    public void execute(GraphNode input) {
-        checkState(input instanceof ScopedGraphNode, "Strategy only accepts ScopedGraphNode");
+    public void execute(GraphNode node) {
+        checkState(node instanceof ScopedGraphNode, "Strategy only accepts ScopedGraphNode");
 
-        ScopedGraphNode closestPrecedingNode = (ScopedGraphNode) input;
+        ScopedGraphNode closestPrecedingNode = (ScopedGraphNode) node;
 
         List<GraphNode> successors = graph.successors(closestPrecedingNode);
 
-        Optional<GraphNode> firstNodeOutsideScope = FindFirstNodeOutsideScope.of(graph, closestPrecedingNode);
-
-        // If isEmpty(successors) or (size(successors) == 1 & successor(closestPrecedingNode) is NodeOutsideScope
         if (successors.isEmpty()) {
             connector.addPredecessor(closestPrecedingNode);
             addToScopeIfNeeded(closestPrecedingNode);
+            connectCommonSuccessorsOf(closestPrecedingNode);
             return;
-        } else {
-
-            if (firstNodeOutsideScope.isPresent()) {
-                GraphNode firstNodeOutside = firstNodeOutsideScope.get();
-                if (successors.size() == 1 && successors.get(0) == firstNodeOutside) {
-                    graph.remove(closestPrecedingNode, firstNodeOutside);
-                    connector.addPredecessor(closestPrecedingNode);
-                    connector.addSuccessor(firstNodeOutside);
-                    addToScopeIfNeeded(closestPrecedingNode);
-                    return;
-                }
-            }
         }
 
         for (int successorIndex = 0; successorIndex < successors.size(); successorIndex++) {
-            // We search where it lies. if it is in the top
-            //
-            // /-----------\ yTopBound
-            // |    TOP    |
-            // |-----------| yBottomBound
-            // |   CENTER  |
-            // |-----------|
-            // |  BOTTOM   |
-            // \-----------/=
-            // We search the node on which the drop point lies on the Y axis.
-            // If one is found, we stop.
+            // |-----------| yTopTopBound
+            // |    top    |
+            // |-----------| yCenterTopBound == yTopBottomBound
+            // |   center  |
+            // |-----------| yCenterBottomBound == yBottomTopBound
+            // |  bottom   |
+            // |-----------| yBottomBottomBound
             GraphNode successor = successors.get(successorIndex);
-            int yTopBound = successor.y() - (Tile.HALF_HEIGHT + Math.floorDiv(Tile.HEIGHT, 4));
-            int yBottomBound = successor.y() - (Tile.HALF_HEIGHT - Math.floorDiv(Tile.HEIGHT, 4));
-
-            if (dropPoint.y > yBottomBound && dropPoint.y < yTopBound) {
+            if (isInsideTopArea(successor, dropPoint)) {
                 connector.addPredecessor(closestPrecedingNode, successorIndex);
                 addToScopeIfNeeded(closestPrecedingNode);
                 connectCommonSuccessorsOf(closestPrecedingNode);
                 return;
-            }
 
-            int yCenterTopBound = yBottomBound;
-            int yCenterBottomBound = successor.y() + (Tile.HALF_HEIGHT - Math.floorDiv(Tile.HEIGHT, 4));
-            if (dropPoint.y <= yCenterBottomBound && yCenterTopBound >= yCenterTopBound) {
+            } else if (isInsideCenterArea(successor, dropPoint)) {
+                // Replaces the first node at index "successorIndex"
                 graph.remove(closestPrecedingNode, successor);
                 connector.addPredecessor(closestPrecedingNode, successorIndex);
                 connector.addSuccessor(successor);
                 addToScopeIfNeeded(closestPrecedingNode);
                 return;
+
+            } else if (isInsideBottomArea(successor, dropPoint)) {
+                connector.addPredecessor(closestPrecedingNode, successorIndex + 1);
+                addToScopeIfNeeded(closestPrecedingNode);
+                connectCommonSuccessorsOf(closestPrecedingNode);
+                return;
             }
         }
+    }
 
-        // This is in the last index, the first node , the start of a new row (route)
-        connector.addPredecessor(closestPrecedingNode);
-        addToScopeIfNeeded(closestPrecedingNode);
-        connectCommonSuccessorsOf(closestPrecedingNode);
+    private boolean isInsideTopArea(GraphNode successor, Point dropPoint) {
+        int yTopTopBound = successor.y() - Tile.HALF_HEIGHT;
+        int yTopBottomBound = successor.y() - (Tile.HALF_HEIGHT - Math.floorDiv(Tile.HEIGHT, 4));
+        return dropPoint.y > yTopTopBound && dropPoint.y < yTopBottomBound;
+    }
+
+    private boolean isInsideCenterArea(GraphNode successor, Point dropPoint) {
+        int yCenterTopBound = successor.y() - (Tile.HALF_HEIGHT - Math.floorDiv(Tile.HEIGHT, 4));
+        int yCenterBottomBound = successor.y() + (Tile.HALF_HEIGHT - Math.floorDiv(Tile.HEIGHT, 4));
+        return dropPoint.y >= yCenterTopBound && dropPoint.y <= yCenterBottomBound;
+    }
+
+    private boolean isInsideBottomArea(GraphNode successor, Point dropPoint) {
+        int yBottomTopBound = successor.y() + (Tile.HALF_HEIGHT - Math.floorDiv(Tile.HEIGHT, 4));
+        int yBottomBottomBound = successor.y() + Tile.HALF_HEIGHT;
+        return dropPoint.y > yBottomTopBound && dropPoint.y < yBottomBottomBound;
     }
 
     private void connectCommonSuccessorsOf(ScopedGraphNode closestPrecedingNode) {
