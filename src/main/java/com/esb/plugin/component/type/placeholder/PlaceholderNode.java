222b1b18a2c21d6cee57a4dae5ff60899688be1f
lorenzo <lorenzo@lorenzo.info.com>
2019-06-25 18:14:17 +0200

    Code

--- src/main/java/com/esb/plugin/component/type/placeholder/PlaceholderNode.java
+++ src/main/java/com/esb/plugin/component/type/placeholder/PlaceholderNode.java
@@ -3,9 +3,23 @@ package com.esb.plugin.component.type.placeholder;
 import com.esb.plugin.component.domain.ComponentData;
 import com.esb.plugin.editor.designer.AbstractGraphNode;
 
+import java.awt.*;
+import java.awt.image.ImageObserver;
+
 public class PlaceholderNode extends AbstractGraphNode {
 
     public PlaceholderNode(ComponentData componentData) {
         super(componentData);
+    }
+
+    @Override
+    protected void drawRemoveComponentIcon(Graphics2D graphics, ImageObserver observer) {
+        // Placeholder cannot be removed
+    }
+
+    @Override
+    protected boolean withinRemoveIcon(int x, int y) {
+        // Placeholder cannot be removed
+        return false;
     }
 }
