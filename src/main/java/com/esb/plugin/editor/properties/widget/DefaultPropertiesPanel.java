6aa51a8caea2776c7ac344f9001aa897b684b3a3
lorenzo <lorenzo@lorenzo.info.com>
2019-07-12 08:10:11 +0200

    script editor

--- src/main/java/com/esb/plugin/editor/properties/widget/DefaultPropertiesPanel.java
+++ src/main/java/com/esb/plugin/editor/properties/widget/DefaultPropertiesPanel.java
@@ -86,4 +86,36 @@ public class DefaultPropertiesPanel extends JBPanel implements DefaultPanelConte
                 .snapshot(snapshot)
                 .build();
     }
+
+    class RendererPropertyAccessorWrapper implements PropertyAccessor {
+
+        private final PropertyAccessor wrapped;
+        private final DefaultPropertiesPanel propertiesPanel;
+
+        RendererPropertyAccessorWrapper(PropertyAccessor wrapped, DefaultPropertiesPanel propertiesPanel) {
+            this.wrapped = wrapped;
+            this.propertiesPanel = propertiesPanel;
+        }
+
+        @Override
+        public FlowSnapshot getSnapshot() {
+            return this.wrapped.getSnapshot();
+        }
+
+        @Override
+        public <T> void set(T object) {
+            this.wrapped.set(object);
+            this.propertiesPanel.notify(wrapped.getProperty(), object);
+        }
+
+        @Override
+        public <T> T get() {
+            return this.wrapped.get();
+        }
+
+        @Override
+        public String getProperty() {
+            return this.wrapped.getProperty();
+        }
+    }
 }
