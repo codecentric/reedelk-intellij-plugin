374662e710d436586eeb62818708b91dfdb8a332
lorenzo <lorenzo@lorenzo.info.com>
2019-08-01 08:16:14 +0200

    bugfix

--- src/main/java/com/esb/plugin/graph/action/add/strategy/NoOpStrategy.java
+++ src/main/java/com/esb/plugin/graph/action/add/strategy/NoOpStrategy.java
@@ -1,5 +1,6 @@
-package com.esb.plugin.graph.action;
+package com.esb.plugin.graph.action.add.strategy;
 
+import com.esb.plugin.graph.action.Strategy;
 import com.esb.plugin.graph.node.GraphNode;
 
 public class NoOpStrategy implements Strategy {
--- src/main/java/com/esb/plugin/graph/action/add/strategy/SubFlowStrategyBuilder.java
+++ src/main/java/com/esb/plugin/graph/action/add/strategy/SubFlowStrategyBuilder.java
@@ -2,7 +2,6 @@ package com.esb.plugin.graph.action.add.strategy;
 
 import com.esb.plugin.commons.Half;
 import com.esb.plugin.graph.FlowGraph;
-import com.esb.plugin.graph.action.NoOpStrategy;
 import com.esb.plugin.graph.action.Strategy;
 import com.esb.plugin.graph.node.GraphNode;
 import com.esb.plugin.graph.utils.FindClosestPrecedingNode;
