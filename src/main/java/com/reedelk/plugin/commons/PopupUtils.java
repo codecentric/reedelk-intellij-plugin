968b6137f7fda216a2021aa15104ee823edcb6d2
lorenzo <lorenzo@lorenzo.info.com>
2019-09-13 08:26:47 +0200

    code

---
+++ src/main/java/com/reedelk/plugin/commons/PopupUtils.java
@@ -0,0 +1,20 @@
+package com.reedelk.plugin.commons;
+
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.awt.RelativePoint;
+
+import javax.swing.*;
+
+import static com.intellij.openapi.ui.MessageType.WARNING;
+import static com.intellij.openapi.ui.popup.Balloon.Position;
+
+public class PopupUtils {
+
+    public static void error(Exception exception, JComponent component) {
+        String errorMessage = exception.getMessage();
+        String content = String.format(Labels.BALLOON_EDIT_CONFIG_ERROR, errorMessage);
+        JBPopupFactory.getInstance().createHtmlTextBalloonBuilder(content, WARNING, null)
+                .createBalloon()
+                .show(RelativePoint.getCenterOf(component), Position.above);
+    }
+}
