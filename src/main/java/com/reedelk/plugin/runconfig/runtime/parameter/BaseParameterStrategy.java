86a288199ba1f64128089fb845877c3fb70d1ad9
lorenzo <lorenzo@lorenzo.info.com>
2019-11-15 15:11:39 +0100

    code

--- src/main/java/com/reedelk/plugin/runconfig/runtime/parameter/BaseParameterStrategy.java
+++ src/main/java/com/reedelk/plugin/runconfig/runtime/parameter/BaseParameterStrategy.java
@@ -1,12 +1,19 @@
 package com.reedelk.plugin.runconfig.runtime.parameter;
 
 import com.intellij.execution.configurations.ParametersList;
+import com.reedelk.plugin.commons.Defaults;
 import com.reedelk.plugin.runconfig.runtime.RuntimeRunConfiguration;
+import com.reedelk.runtime.api.commons.StringUtils;
 
 abstract class BaseParameterStrategy implements ParameterStrategy {
 
     @Override
     public void apply(ParametersList parameters, RuntimeRunConfiguration configuration) {
-        parameters.add("-Dadmin.console.bind.port=" + configuration.getRuntimePort());
+        if (StringUtils.isNotBlank(configuration.getRuntimePort())) {
+            parameters.add("-D" + Defaults.DEFAULT_ADMIN_PORT_PARAM_NAME + "=" + configuration.getRuntimePort());
+        }
+        if (StringUtils.isNotBlank(configuration.getRuntimeBindAddress())) {
+            parameters.add("-D" + Defaults.DEFAULT_ADMIN_HOST_PARAM_NAME + "=" + configuration.getRuntimeBindAddress());
+        }
     }
 }
