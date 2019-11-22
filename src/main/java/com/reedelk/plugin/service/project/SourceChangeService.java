5ea42b5b7c820e26aa869a839d30218de91016ef
lorenzo <lorenzo@lorenzo.info.com>
2019-11-22 11:38:00 +0100

    code

--- src/main/java/com/reedelk/plugin/service/project/SourceChangeService.java
+++ src/main/java/com/reedelk/plugin/service/project/SourceChangeService.java
@@ -1,13 +1,12 @@
 package com.reedelk.plugin.service.project;
 
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public interface SourceChangeService {
 
     static SourceChangeService getInstance(@NotNull Project project) {
-        return ServiceManager.getService(project, SourceChangeService.class);
+        return project.getComponent(SourceChangeService.class);
     }
 
     boolean isHotSwap(String runtimeConfigName, String moduleName);
