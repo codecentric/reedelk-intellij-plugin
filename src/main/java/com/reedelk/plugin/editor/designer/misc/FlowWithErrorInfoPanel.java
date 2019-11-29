68c0c8702c68818f6b4bb7ff3004d0c257fa7f34
lorenzo <lorenzo@lorenzo.info.com>
2019-11-29 10:35:33 +0100

    code

--- src/main/java/com/reedelk/plugin/editor/designer/misc/FlowWithErrorInfoPanel.java
+++ src/main/java/com/reedelk/plugin/editor/designer/misc/FlowWithErrorInfoPanel.java
@@ -1,12 +1,14 @@
 package com.reedelk.plugin.editor.designer.misc;
 
-import com.reedelk.plugin.commons.Half;
-import com.reedelk.plugin.commons.Images;
+import com.reedelk.plugin.commons.*;
+import com.reedelk.plugin.editor.designer.text.AbstractText;
 import com.reedelk.plugin.graph.FlowGraph;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.reedelk.plugin.commons.Messages.Flow;
 
@@ -26,5 +28,26 @@ public class FlowWithErrorInfoPanel extends BaseInfoPanel {
         FlowErrorText flowErrorText = new FlowErrorText(message);
         flowErrorText.setPosition(Half.of(parent.getWidth()), yCoordinateStart);
         flowErrorText.draw(g2);
+    }
+
+    static class FlowErrorText extends AbstractText {
+
+        private static final Pattern REGEX = Pattern.compile(".{1,110}(?:\\s|$)", Pattern.DOTALL);
+        private final String text;
+
+        FlowErrorText(String text) {
+            super(Fonts.Component.DESCRIPTION, HorizontalAlignment.CENTER, VerticalAlignment.BELOW);
+            this.text = text;
+        }
+
+        @Override
+        protected Color getColor() {
+            return Colors.PROPERTIES_EMPTY_SELECTION_TEXT;
+        }
+
+        @Override
+        protected List<String> getText() {
+            return SplitTextInLines.from(text, REGEX);
+        }
     }
 }
