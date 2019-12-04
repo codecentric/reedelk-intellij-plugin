7788df97ba16aa30daca5fe68a61edee1d40c02d
lorenzo <lorenzo@lorenzo.info.com>
2019-12-04 10:57:40 +0100

    code

---
+++ src/main/java/com/reedelk/plugin/commons/FlowErrorResponse.java
@@ -0,0 +1,24 @@
+package com.reedelk.plugin.commons;
+
+import com.reedelk.runtime.api.commons.FlowError;
+import org.json.JSONObject;
+
+import java.util.Optional;
+
+public class FlowErrorResponse {
+
+    public static Optional<FlowError> from(String httpResponseBody) {
+        try {
+            JSONObject object = new JSONObject(httpResponseBody);
+            long moduleId = object.has(FlowError.Properties.moduleId) ? object.getLong(FlowError.Properties.moduleId) : -1;
+            String moduleName = object.has(FlowError.Properties.moduleName) ? object.getString(FlowError.Properties.moduleName) : null;
+            String flowId = object.has(FlowError.Properties.flowId) ? object.getString(FlowError.Properties.flowId) : null;
+            String flowTitle = object.has(FlowError.Properties.flowTitle) ? object.getString(FlowError.Properties.flowTitle) : null;
+            String errorType = object.has(FlowError.Properties.errorType) ? object.getString(FlowError.Properties.errorType) : null;
+            String errorMessage = object.has(FlowError.Properties.errorMessage) ? object.getString(FlowError.Properties.errorMessage) : null;
+            return Optional.of(new FlowError(moduleId, moduleName, flowId, flowTitle, errorType, errorMessage));
+        } catch (Exception exception) {
+            return Optional.empty();
+        }
+    }
+}
--- src/main/java/com/reedelk/plugin/runconfig/module/runprofile/RestService.java
+++ src/main/java/com/reedelk/plugin/runconfig/module/runprofile/RestService.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.reedelk.plugin.commons.FlowErrorResponse;
 import com.reedelk.plugin.maven.MavenPackageGoal;
 import com.reedelk.plugin.service.application.HttpResponse;
 import com.reedelk.plugin.service.application.HttpService;
@@ -15,14 +16,21 @@ import com.reedelk.runtime.rest.api.module.v1.ModulePOSTReq;
 
 import java.io.IOException;
 
+import static com.reedelk.plugin.message.ReedelkBundle.message;
+
 public class RestService {
 
     private static final Logger LOG = Logger.getInstance(RestService.class);
-
     private static final String BASE_ADMIN_CONSOLE_URL_TEMPLATE = "http://%s:%d/api";
+
+    private interface ApiPaths {
+        String module = "/module";
+        String hotswap = "/hotswap";
+    }
+
+    private final Module module;
     private final String baseUrl;
     private final Project project;
-    private final Module module;
 
     RestService(Project project, Module module, String address, int port) {
         this.baseUrl = String.format(BASE_ADMIN_CONSOLE_URL_TEMPLATE, address, port);
@@ -37,8 +45,7 @@ public class RestService {
 
         String json = InternalAPI.HotSwap.V1.POST.Req.serialize(req);
 
-
-        HttpResponse response = post(baseUrl + "/hotswap", json);
+        HttpResponse response = post(baseUrl + ApiPaths.hotswap, json);
         if (response.isNotFound()) {
             // The module we tried to Hot Swap was not installed
             // in the Runtime, therefore we must re-package the module
@@ -46,51 +53,42 @@ public class RestService {
             // It is important to re-package the module since the files supposed to be
             // hot swapped are not in the jar yet.
             MavenPackageGoal packageGoal = new MavenPackageGoal(project, module.getName(), result -> {
-                try {
-                    if (result) deploy(moduleFile);
-                    else LOG.error("Deploy error", "Maven goal 'package' failed");
-                } catch (ExecutionException e) {
-                    LOG.error("Deploy error", e);
+                if (!result) {
+                    // Maven package goal was not successful
+                    LOG.warn(message("module.run.error.maven.goal.package.failed", module.getName()));
+                } else {
+                    try {
+                        // The  Maven package goal was successful. The .jar artifact is in the /target folder
+                        // and we can deploy the package onto the ESB runtime.
+                        deploy(moduleFile);
+                    } catch (ExecutionException e) {
+                        LOG.warn(message("module.run.error.maven.goal.package.failed", module.getName()));
+                    }
                 }
             });
-
             packageGoal.execute();
-
         } else if (response.isNotSuccessful()) {
-            LOG.error("HotSwap error", response.getBody());
-            throw new ExecutionException(response.getBody());
+            handleNotSuccessfulResponse(response);
         }
     }
 
     void deploy(String moduleFile) throws ExecutionException {
         ModulePOSTReq req = new ModulePOSTReq();
         req.setModuleFilePath(moduleFile);
-
         String json = InternalAPI.Module.V1.POST.Req.serialize(req);
-
-
-        HttpResponse response = post(baseUrl + "/module", json);
+        HttpResponse response = post(baseUrl + ApiPaths.module, json);
         if (response.isNotSuccessful()) {
-            LOG.error("Deploy error", response.getBody());
-
-            // TODO: Formalize this response (as JSON)
-            throw new ExecutionException(response.getBody());
+            handleNotSuccessfulResponse(response);
         }
     }
 
     public void delete(String moduleFile) throws ExecutionException {
         ModuleDELETEReq req = new ModuleDELETEReq();
-
         req.setModuleFilePath(moduleFile);
-
         String json = InternalAPI.Module.V1.DELETE.Req.serialize(req);
-
-        HttpResponse response = delete(baseUrl + "/module", json);
+        HttpResponse response = delete(baseUrl + ApiPaths.module, json);
         if (response.isNotSuccessful()) {
-            LOG.error("Delete error", response.getBody());
-
-            // TODO: Formalize this response (as JSON)
-            throw new ExecutionException(response.getBody());
+            handleNotSuccessfulResponse(response);
         }
     }
 
@@ -110,5 +108,12 @@ public class RestService {
         } catch (IOException e) {
             throw new ExecutionException(e);
         }
+    }
+
+    private void handleNotSuccessfulResponse(HttpResponse response) throws ExecutionException {
+        LOG.warn(response.getBody());
+        throw FlowErrorResponse.from(response.getBody())
+                .map(flowError -> new ExecutionException(flowError.getErrorMessage()))
+                .orElse(new ExecutionException(response.getBody()));
     }
 }
