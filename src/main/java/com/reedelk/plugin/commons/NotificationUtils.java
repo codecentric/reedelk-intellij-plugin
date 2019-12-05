3a57e2cc5af7066104b56b374a41c81c82620542
lorenzo <lorenzo@lorenzo.info.com>
2019-12-05 13:23:48 +0100

    code

--- src/main/java/com/reedelk/plugin/commons/NotificationUtils.java
+++ src/main/java/com/reedelk/plugin/commons/NotificationUtils.java
@@ -6,30 +6,31 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ToolWindowManager;
 
+import javax.swing.*;
+
 public class NotificationUtils {
 
     private NotificationUtils() {
     }
 
     public static void notifyInfo(final String toolWindowId, final String text, final Project project) {
-        ToolWindowManager
-                .getInstance(project)
-                .notifyByBalloon(toolWindowId, MessageType.INFO, text);
+        SwingUtilities.invokeLater(() -> ToolWindowManager.getInstance(project)
+                .notifyByBalloon(toolWindowId, MessageType.INFO, text));
 
     }
 
     public static void notifyWarn(final String text, final Project project) {
-        ToolWindowManager.getInstance(project)
-                .notifyByBalloon(ToolWindowId.RUN, MessageType.WARNING, text);
+        SwingUtilities.invokeLater(() -> ToolWindowManager.getInstance(project)
+                .notifyByBalloon(ToolWindowId.RUN, MessageType.WARNING, text));
     }
 
     public static void notifyError(final String text, final Project project) {
-        ToolWindowManager.getInstance(project)
-                .notifyByBalloon(ToolWindowId.RUN, MessageType.ERROR, StringUtil.notNullize(text, "internal error"));
+        SwingUtilities.invokeLater(() -> ToolWindowManager.getInstance(project)
+                .notifyByBalloon(ToolWindowId.RUN, MessageType.ERROR, StringUtil.notNullize(text, "internal error")));
     }
 
     public static void notifyError(final Exception ex, final Project project) {
-        ToolWindowManager.getInstance(project)
-                .notifyByBalloon(ToolWindowId.RUN, MessageType.ERROR, StringUtil.notNullize(ex.getMessage(), "internal error"));
+        SwingUtilities.invokeLater(() -> ToolWindowManager.getInstance(project)
+                .notifyByBalloon(ToolWindowId.RUN, MessageType.ERROR, StringUtil.notNullize(ex.getMessage(), "internal error")));
     }
 }
