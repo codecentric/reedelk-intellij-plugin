3675c77a4e3c0e6ac6dbdb466817219ddb2c6cac
lorenzo <lorenzo@lorenzo.info.com>
2019-12-05 17:38:51 +0100

    code

--- src/main/java/com/reedelk/plugin/commons/ToolWindowUtils.java
+++ src/main/java/com/reedelk/plugin/commons/ToolWindowUtils.java
@@ -4,8 +4,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowId;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.content.Content;
 import com.reedelk.plugin.editor.properties.PropertiesPanelToolWindowFactory;
-import com.reedelk.runtime.api.commons.StringUtils;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.util.Optional;
@@ -18,19 +19,43 @@ public class ToolWindowUtils {
     }
 
     public static void switchToolWindowAndNotifyWithMessage(Project project, String message, String runConfigName) {
-        SwingUtilities.invokeLater(() -> {
-            NotificationUtils.notifyInfo(ToolWindowId.RUN, message, project);
-            get(project, ToolWindowId.RUN).ifPresent(window ->
-                    stream(window.getContentManager().getContents()).forEach(content -> {
-                        if (StringUtils.isNotBlank(runConfigName)) {
-                            if (runConfigName.equals(content.getDisplayName())) {
-                                window.getContentManager().setSelectedContent(content, false);
-                                window.show(() -> {
-                                });
-                            }
-                        }
-                    }));
-        });
+        SwingUtilities.invokeLater(() ->
+                findToolWindowByRunConfig(project, runConfigName).ifPresent(toolWindow -> {
+                    getContentBy(toolWindow.toolWindow, runConfigName).ifPresent(content ->
+                            toolWindow.toolWindow.getContentManager().setSelectedContent(content));
+                    toolWindow.toolWindow.show(() ->
+                            NotificationUtils.notifyInfo(toolWindow.toolWindowId, message, project));
+                }));
+    }
+
+    private static Optional<ToolWindowAndId> findToolWindowByRunConfig(@NotNull Project project, @NotNull String runConfigName) {
+
+        Optional<Content> contentInRunToolWindow = get(project, ToolWindowId.RUN)
+                .flatMap(toolWindow -> getContentBy(toolWindow, runConfigName));
+        if (contentInRunToolWindow.isPresent()) return get(project, ToolWindowId.RUN)
+                .map(toolWindow -> new ToolWindowAndId(toolWindow, ToolWindowId.RUN));
+
+        Optional<Content> contentInDebugToolWindow = get(project, ToolWindowId.DEBUG)
+                .flatMap(toolWindow -> getContentBy(toolWindow, runConfigName));
+        if (contentInDebugToolWindow.isPresent()) return get(project, ToolWindowId.DEBUG)
+                .map(toolWindow -> new ToolWindowAndId(toolWindow, ToolWindowId.DEBUG));
+
+        return Optional.empty();
+    }
+
+    static class ToolWindowAndId {
+        ToolWindow toolWindow;
+        String toolWindowId;
+
+        ToolWindowAndId(ToolWindow toolWindow, String toolWindowId) {
+            this.toolWindow = toolWindow;
+            this.toolWindowId = toolWindowId;
+        }
+    }
+
+    private static Optional<Content> getContentBy(ToolWindow toolWindow, @NotNull String contentDisplayName) {
+        return stream(toolWindow.getContentManager().getContents())
+                .filter(content -> contentDisplayName.equals(content.getDisplayName())).findFirst();
     }
 
     public static void show(Project project) {
