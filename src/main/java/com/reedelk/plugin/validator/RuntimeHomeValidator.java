a47ec878aab1428669de9a95e1b651081f65f0cf
lorenzo <lorenzo@lorenzo.info.com>
2019-12-10 21:47:10 +0100

    code

--- src/main/java/com/reedelk/plugin/validator/RuntimeHomeValidator.java
+++ src/main/java/com/reedelk/plugin/validator/RuntimeHomeValidator.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
 
-import static java.lang.String.format;
+import static com.reedelk.plugin.message.ReedelkBundle.message;
 
 public class RuntimeHomeValidator implements Validator {
 
@@ -22,44 +22,44 @@ public class RuntimeHomeValidator implements Validator {
     public void validate(Collection<String> errors) {
         File homeDirectoryFile = new File(runtimeHomeDirectory);
         if (!homeDirectoryFile.exists()) {
-            errors.add("Runtime home directory does not exists");
+            errors.add(message("runtimeBuilder.runtime.home.validator.dir.does.not.exists"));
             return;
         }
 
         if (!homeDirectoryFile.isDirectory()) {
-            errors.add("Runtime home directory is not a directory");
+            errors.add(message("runtimeBuilder.runtime.home.validator.dir.is.not.directory"));
             return;
         }
 
         String[] content = homeDirectoryFile.list();
         if (content == null) {
-            errors.add("Runtime home directory");
+            errors.add(message("runtimeBuilder.runtime.home.validator.generic"));
             return;
         }
 
         // Home directory must contain: modules/config
         boolean existsConfigDir = existsDirectoryNamed(content, "config");
         if (!existsConfigDir) {
-            errors.add("Runtime home directory does not contain 'config' folder");
+            errors.add(message("runtimeBuilder.runtime.home.validator.config.missing"));
             return;
         }
 
         boolean existsModulesDir = existsDirectoryNamed(content, "modules");
         if (!existsModulesDir) {
-            errors.add("Runtime home directory does not contain 'modules' folder");
+            errors.add(message("runtimeBuilder.runtime.home.validator.modules.missing"));
             return;
         }
 
         boolean existsBinDir = existsDirectoryNamed(content, "bin");
         if (!existsBinDir) {
-            errors.add("Runtime home directory does not contain 'bin' folder");
+            errors.add(message("runtimeBuilder.runtime.home.validator.bin.missing"));
             return;
         }
 
         // Check that exists runtime jar.
         Optional<String> runtimeJarName = FileUtils.findRuntimeJarName(runtimeHomeDirectory);
         if (!runtimeJarName.isPresent()) {
-            errors.add(format("Could not find suitable runtime (home directory: %s)", runtimeHomeDirectory));
+            errors.add(message("runtimeBuilder.runtime.home.validator.runtime.jar.missing", runtimeHomeDirectory));
         }
 
         // Runtime Home Directory is valid
