d617b21f95e23406eb01f3830c2ac0e8eea22445
lorenzo <lorenzo@lorenzo.info.com>
2019-12-12 11:40:38 +0100

    code

--- src/test/java/com/reedelk/plugin/commons/ScriptFunctionDefinitionBuilderTest.java
+++ src/test/java/com/reedelk/plugin/commons/ScriptFunctionDefinitionBuilderTest.java
@@ -1,12 +1,48 @@
 package com.reedelk.plugin.commons;
 
+import com.reedelk.plugin.component.domain.ScriptSignatureDefinition;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ScriptFunctionDefinitionBuilderTest {
 
     @Test
-    void shouldDoSomething() {
+    void shouldBuildCorrectlyBuildFunctionDefinitionWhenArgumentsGreaterThanZero() {
+        // Given
+        String expectedFunction = "function myTestFunction(context,arg1,arg2,arg3) {\n" +
+                "\tvar text = 'myTestFunction function';\n" +
+                "\treturn text + ' result';\n" +
+                "}";
+        String  scriptFunctionName = "myTestFunction";
+        ScriptSignatureDefinition definition =
+                new ScriptSignatureDefinition(Arrays.asList("context", "arg1", "arg2", "arg3"));
 
+        // When
+        String scriptFunctionDefinition = ScriptFunctionDefinitionBuilder.from(scriptFunctionName, definition);
+
+        // Then
+        assertThat(scriptFunctionDefinition).isEqualTo(expectedFunction);
     }
 
+    @Test
+    void shouldCorrectlyBuildFunctionDefinitionWhenArgumentsAreEmpty() {
+        // Given
+        String expectedFunction = "function myTestFunction() {\n" +
+                "\tvar text = 'myTestFunction function';\n" +
+                "\treturn text + ' result';\n" +
+                "}";
+        String  scriptFunctionName = "myTestFunction";
+        ScriptSignatureDefinition definition =
+                new ScriptSignatureDefinition(Collections.emptyList());
+
+        // When
+        String scriptFunctionDefinition = ScriptFunctionDefinitionBuilder.from(scriptFunctionName, definition);
+
+        // Then
+        assertThat(scriptFunctionDefinition).isEqualTo(expectedFunction);
+    }
 }
