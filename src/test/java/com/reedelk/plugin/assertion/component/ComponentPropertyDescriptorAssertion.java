16969eac95a077a7c3f10b94677eeb86ee239856
lorenzo <lorenzo@lorenzo.info.com>
2019-12-12 11:52:53 +0100

    code

--- src/test/java/com/reedelk/plugin/assertion/component/ComponentPropertyDescriptorAssertion.java
+++ src/test/java/com/reedelk/plugin/assertion/component/ComponentPropertyDescriptorAssertion.java
@@ -2,11 +2,13 @@ package com.reedelk.plugin.assertion.component;
 
 import com.reedelk.plugin.component.domain.AutoCompleteContributorDefinition;
 import com.reedelk.plugin.component.domain.ComponentPropertyDescriptor;
+import com.reedelk.plugin.component.domain.ScriptSignatureDefinition;
 import com.reedelk.plugin.component.domain.TypeDescriptor;
 
 import java.util.Optional;
 
 import static com.reedelk.plugin.assertion.component.AutoCompleteContributorDefinitionMatchers.AutoCompleteContributorDefinitionMatcher;
+import static com.reedelk.plugin.assertion.component.ScriptSignatureDefinitionMatchers.ScriptSignatureDefinitionMatcher;
 import static com.reedelk.plugin.assertion.component.TypeDescriptorMatchers.TypeDescriptorMatcher;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,6 +50,19 @@ public class ComponentPropertyDescriptorAssertion {
 
     public ComponentPropertyDescriptorAssertion hasNotAutoCompleteContributorDefinition() {
         Optional<AutoCompleteContributorDefinition> definition = propertyDescriptor.getAutoCompleteContributorDefinition();
+        assertThat(definition).isNotPresent();
+        return this;
+    }
+
+    public ComponentPropertyDescriptorAssertion hasScriptSignatureDefinition(ScriptSignatureDefinitionMatcher matcher) {
+        Optional<ScriptSignatureDefinition> definition = propertyDescriptor.getScriptSignatureDefinition();
+        assertThat(definition).isPresent();
+        assertThat(matcher.matches(definition.get())).isTrue();
+        return this;
+    }
+
+    public ComponentPropertyDescriptorAssertion hasNotScriptSignatureDefinition() {
+        Optional<ScriptSignatureDefinition> definition = propertyDescriptor.getScriptSignatureDefinition();
         assertThat(definition).isNotPresent();
         return this;
     }
