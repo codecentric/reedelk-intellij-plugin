e7ee08516a9995b0d4cc35ccbb54ea34918a422a
lorenzo <lorenzo@lorenzo.info.com>
2019-12-16 08:27:59 +0100

    code

---
+++ src/main/java/com/reedelk/plugin/commons/Versions.java
@@ -0,0 +1,18 @@
+package com.reedelk.plugin.commons;
+
+import com.intellij.openapi.util.Version;
+import org.jetbrains.annotations.Nullable;
+
+public class Versions {
+
+    public static boolean compare(@Nullable Version moduleVersion, @Nullable Version artifactVersion) {
+        if (moduleVersion == null) {
+            return artifactVersion == null;
+        } else if (artifactVersion == null) {
+            // Module version would be != null.
+            return false;
+        } else {
+            return moduleVersion.compareTo(artifactVersion) == 0;
+        }
+    }
+}
--- src/main/java/com/reedelk/plugin/service/module/impl/modulesync/ModuleSyncServiceImpl.java
+++ src/main/java/com/reedelk/plugin/service/module/impl/modulesync/ModuleSyncServiceImpl.java
@@ -4,13 +4,13 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.Version;
 import com.reedelk.plugin.commons.ExcludedArtifactsFromModuleSync;
+import com.reedelk.plugin.commons.Versions;
 import com.reedelk.plugin.executor.PluginExecutors;
 import com.reedelk.plugin.maven.MavenUtils;
 import com.reedelk.plugin.service.module.ModuleSyncService;
 import com.reedelk.plugin.service.module.RuntimeApiService;
 import com.reedelk.runtime.commons.ModuleUtils;
 import com.reedelk.runtime.rest.api.module.v1.ModuleGETRes;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.idea.maven.model.MavenArtifact;
 import org.jetbrains.idea.maven.model.MavenArtifactNode;
 import org.jetbrains.idea.maven.model.MavenConstants;
@@ -23,7 +23,6 @@ import java.util.Optional;
 
 import static com.reedelk.plugin.message.ReedelkBundle.message;
 import static com.reedelk.plugin.service.module.RuntimeApiService.OperationCallback;
-import static com.reedelk.runtime.commons.Preconditions.checkState;
 
 public class ModuleSyncServiceImpl implements ModuleSyncService {
 
@@ -83,7 +82,7 @@ public class ModuleSyncServiceImpl implements ModuleSyncService {
             Version moduleVersion = Version.parseVersion(moduleDTO.getVersion());
 
             // All good, they have the same version, no need to install the artifact.
-            if (sameVersion(moduleVersion, artifactVersion)) {
+            if (Versions.compare(moduleVersion, artifactVersion)) {
                 return;
             }
         }
@@ -93,12 +92,6 @@ public class ModuleSyncServiceImpl implements ModuleSyncService {
         if (isModule(artifact.getFile())) {
             installModuleArtifactIntoRuntime(runtimeHostAddress, runtimeHostPort, artifact);
         }
-    }
-
-    private boolean sameVersion(@Nullable Version moduleVersion, @Nullable Version artifactVersion) {
-        checkState(moduleVersion != null, "module version");
-        checkState(artifactVersion != null, "artifact version");
-        return moduleVersion.compareTo(artifactVersion) == 0;
     }
 
     private Optional<ModuleGETRes> findInstalledModuleByName(Collection<ModuleGETRes> installedModules, String moduleName) {
