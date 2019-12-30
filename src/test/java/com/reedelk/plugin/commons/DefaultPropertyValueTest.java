768063007b260c68ae39550b5f9f27d298feebc1
lorenzo <lorenzo@lorenzo.info.com>
2019-12-30 20:42:23 +0100

    code

--- src/test/java/com/reedelk/plugin/commons/DefaultPropertyValueTest.java
+++ src/test/java/com/reedelk/plugin/commons/DefaultPropertyValueTest.java
@@ -1,19 +1,47 @@
 package com.reedelk.plugin.commons;
 
+import com.reedelk.module.descriptor.model.ComponentPropertyDescriptor;
+import com.reedelk.module.descriptor.model.TypePrimitiveDescriptor;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultPropertyValueTest {
 
-    // TODO: Complete me
     @Test
-    void should() {
+    void shouldUseDefaultValueWhenDefaultIsNotSpecified() {
         // Given
-        //ComponentPropertyDescriptor
+        TypePrimitiveDescriptor typeDescriptor = new TypePrimitiveDescriptor();
+        typeDescriptor.setType(long.class);
+
+        ComponentPropertyDescriptor descriptor = ComponentPropertyDescriptor.builder()
+                .propertyName("property1")
+                .type(typeDescriptor)
+                .build();
 
         // When
-        //DefaultPropertyValue.of()
+        Object actual = DefaultPropertyValue.of(descriptor);
 
         // Then
+        assertThat(actual).isEqualTo(0L);
     }
 
+    @Test
+    void shouldConvertValueFromDefaultValueString() {
+        // Given
+        TypePrimitiveDescriptor typeDescriptor = new TypePrimitiveDescriptor();
+        typeDescriptor.setType(long.class);
+
+        ComponentPropertyDescriptor descriptor = ComponentPropertyDescriptor.builder()
+                .propertyName("property1")
+                .type(typeDescriptor)
+                .defaultValue("98881")
+                .build();
+
+        // When
+        Object actual = DefaultPropertyValue.of(descriptor);
+
+        // Then
+        assertThat(actual).isEqualTo(98881L);
+    }
 }
