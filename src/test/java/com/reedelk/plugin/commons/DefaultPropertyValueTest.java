b761eb66df7051b528542f5a832785e6601692a0
lorenzo <lorenzo@lorenzo.info.com>
2019-12-31 10:13:59 +0100

    code

--- src/test/java/com/reedelk/plugin/commons/DefaultPropertyValueTest.java
+++ src/test/java/com/reedelk/plugin/commons/DefaultPropertyValueTest.java
@@ -2,6 +2,7 @@ package com.reedelk.plugin.commons;
 
 import com.reedelk.module.descriptor.model.ComponentPropertyDescriptor;
 import com.reedelk.module.descriptor.model.TypePrimitiveDescriptor;
+import com.reedelk.runtime.api.annotation.Default;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +25,25 @@ class DefaultPropertyValueTest {
 
         // Then
         assertThat(actual).isEqualTo(0L);
+    }
+
+    @Test
+    void shouldUseDefaultValueWhenDefaultIsDefault_USE_DEFAULT_VALUE() {
+        // Given
+        TypePrimitiveDescriptor typeDescriptor = new TypePrimitiveDescriptor();
+        typeDescriptor.setType(int.class);
+
+        ComponentPropertyDescriptor descriptor = ComponentPropertyDescriptor.builder()
+                .propertyName("property1")
+                .defaultValue(Default.USE_DEFAULT_VALUE)
+                .type(typeDescriptor)
+                .build();
+
+        // When
+        Object actual = DefaultPropertyValue.of(descriptor);
+
+        // Then
+        assertThat(actual).isEqualTo(0);
     }
 
     @Test
