7ef242fdae2293b23323a59ad0838e0969f87f02
lorenzo <lorenzo@lorenzo.info.com>
2020-01-20 18:27:31 +0100

    code

--- src/main/java/com/reedelk/plugin/runconfig/runtime/RuntimeRunCommandLine.java
+++ src/main/java/com/reedelk/plugin/runconfig/runtime/RuntimeRunCommandLine.java
@@ -7,12 +7,12 @@ import com.intellij.execution.configurations.ParametersList;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
-import com.reedelk.plugin.commons.FileUtils;
 import com.reedelk.plugin.commons.ProjectSdk;
 import com.reedelk.plugin.runconfig.runtime.parameter.ParameterStrategyBuilder;
 import com.reedelk.runtime.commons.FileExtension;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -46,22 +46,15 @@ public class RuntimeRunCommandLine extends JavaCommandLineState {
         String runtimeHomeDirectory = configuration.getRuntimeHomeDirectory();
 
         Path libsPath = Paths.get(runtimeHomeDirectory,
-                NameConvention.RUNTIME_PACKAGE_LIB_DIRECTORY, "*");
+                NameConvention.RUNTIME_PACKAGE_LIB_DIRECTORY);
         Path runtimePath = Paths.get(runtimeHomeDirectory,
                 NameConvention.RUNTIME_PACKAGE_BIN_DIRECTORY,
                 NameConvention.RUNTIME_JAR_FILE_PREFIX + "." + FileExtension.JAR.value());
 
         javaParams.setJdk(sdk);
-        javaParams.getClassPath().add(libsPath.toString());
+        javaParams.getClassPath().add(libsPath.toString() + File.separator +  "*");
         javaParams.getClassPath().add(runtimePath.toString());
         javaParams.setMainClass(NameConvention.RUNTIME_LAUNCHER_CLASS);
         return javaParams;
-    }
-
-    private String getJarPath(String runtimeHomeDirectory) throws ExecutionException {
-        String runtimeJarName = FileUtils
-                .findRuntimeJarName(runtimeHomeDirectory)
-                .orElseThrow(() -> new ExecutionException(message("runtime.run.error.runtime.jar.not.found", runtimeHomeDirectory)));
-        return Paths.get(runtimeHomeDirectory, NameConvention.RUNTIME_PACKAGE_BIN_DIRECTORY, runtimeJarName).toString();
     }
 }
