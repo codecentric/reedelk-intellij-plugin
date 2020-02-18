feb5310153be4a22e3d9784870c51ba29dc9fcc4
lorenzo <lorenzo@lorenzo.info.com>
2020-02-18 17:41:14 +0100

    code

---
+++ src/test/java/com/reedelk/plugin/runconfig/runtime/parameter/BaseParameterStrategyTest.java
@@ -0,0 +1,79 @@
+package com.reedelk.plugin.runconfig.runtime.parameter;
+
+import com.intellij.execution.configurations.ParametersList;
+import com.reedelk.plugin.runconfig.runtime.RuntimeRunConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class BaseParameterStrategyTest {
+
+    @Mock
+    private RuntimeRunConfiguration runtimeRunConfiguration;
+
+    private TestParameterStrategy strategy;
+
+    @BeforeEach
+    void setUp() {
+         strategy = new TestParameterStrategy();
+    }
+
+    @Test
+    void shouldCorrectlyConfigureAdditionalVMOptions() {
+        // Given
+        String additionalVMOption = "-Dtest.port.overridden=22331";
+        doReturn(additionalVMOption).when(runtimeRunConfiguration).getVmOptions();
+
+        ParametersList parametersList = new ParametersList();
+
+        // When
+        strategy.apply(parametersList, runtimeRunConfiguration);
+
+        // Then
+        List<String> list = parametersList.getList();
+        assertThat(list).containsExactly("-Dtest.port.overridden=22331");
+    }
+
+    @Test
+    void shouldCorrectlyConfigureAdminConsolePort() {
+        // Given
+        String adminConsolePort = "9911";
+        doReturn(adminConsolePort).when(runtimeRunConfiguration).getRuntimePort();
+
+        ParametersList parametersList = new ParametersList();
+
+        // When
+        strategy.apply(parametersList, runtimeRunConfiguration);
+
+        // Then
+        List<String> list = parametersList.getList();
+        assertThat(list).containsExactly("-Dadmin.console.port=9911");
+    }
+
+    @Test
+    void shouldCorrectlyConfigureAdminConsoleHost() {
+        // Given
+        String adminConsoleHost = "127.0.0.1";
+        doReturn(adminConsoleHost).when(runtimeRunConfiguration).getRuntimeBindAddress();
+
+        ParametersList parametersList = new ParametersList();
+
+        // When
+        strategy.apply(parametersList, runtimeRunConfiguration);
+
+        // Then
+        List<String> list = parametersList.getList();
+        assertThat(list).containsExactly("-Dadmin.console.address=127.0.0.1");
+    }
+
+    static class TestParameterStrategy extends BaseParameterStrategy {
+    }
+}
