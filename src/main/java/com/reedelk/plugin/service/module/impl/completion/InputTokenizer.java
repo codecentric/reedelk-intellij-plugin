4433e54acfaa6020bd4aca3b3f40d7af4ec262f7
lorenzo <lorenzo@lorenzo.info.com>
2020-02-28 17:21:42 +0100

    code

--- src/main/java/com/reedelk/plugin/service/module/impl/completion/InputTokenizer.java
+++ src/main/java/com/reedelk/plugin/service/module/impl/completion/InputTokenizer.java
@@ -12,13 +12,25 @@ public class InputTokenizer {
         char c;
         for (int i = 0; i < input.length(); i++) {
             c = input.charAt(i);
-
             if (c == '(') {
                 stack.push(c);
             } else if (c == ')') {
                 stack.pop();
             } else if (stack.empty() && c != ';') {
                 builder.append(c);
+            }
+        }
+
+        // If stack is not empty
+        if (!stack.isEmpty()) {
+            // We need to go back
+            int i = input.length() - 1;
+            while (i > 0) {
+                char c1 = input.charAt(i);
+                if (c1 == '(') {
+                    return input.substring(i+1).split("\\.");
+                }
+                i--;
             }
         }
 
