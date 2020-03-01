f58798bd6b9944f2db00f809b928a219ed6355c2
lorenzo <lorenzo@lorenzo.info.com>
2020-03-01 11:25:51 +0100

    code

--- src/test/java/com/reedelk/plugin/service/module/impl/completion/InputTokenizerTest.java
+++ src/test/java/com/reedelk/plugin/service/module/impl/completion/InputTokenizerTest.java
@@ -7,7 +7,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 class InputTokenizerTest {
 
     @Test
-    void shouldCorrectlyTokenize() {
+    void shouldCorrectlyTokenizeRootVariable() {
+        // Given
+        String input = "MultipartBuilder";
+
+        // When
+        String[] result = InputTokenizer.tokenize(input);
+
+        // Then
+        assertThat(result).hasSize(1);
+        assertThat(result).contains("MultipartBuilder");
+    }
+
+    @Test
+    void shouldCorrectlyTokenizeEmptyString() {
+        // Given
+        String input = "";
+
+        // When
+        String[] result = InputTokenizer.tokenize(input);
+
+        // Then
+        assertThat(result).containsExactly("");
+    }
+
+    @Test
+    void shouldCorrectlyTokenizeBuilderString() {
         // Given
         String input = "MultipartBuilder.part('image').binary(message.payload());";
 
