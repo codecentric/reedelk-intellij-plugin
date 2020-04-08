1da574d250c25bf3a130b513bd0ff57b35ad4eec
lorenzo <lorenzo@lorenzo.info.com>
2020-04-08 18:20:53 +0200

    code

--- src/main/java/com/reedelk/plugin/editor/properties/commons/DefaultObjectTypeContainer.java
+++ src/main/java/com/reedelk/plugin/editor/properties/commons/DefaultObjectTypeContainer.java
@@ -7,13 +7,12 @@ import javax.swing.border.Border;
 import java.awt.*;
 
 import static com.intellij.util.ui.JBUI.Borders.empty;
-import static com.intellij.util.ui.JBUI.Borders.emptyTop;
 import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.NORTH;
 
 public class DefaultObjectTypeContainer extends DisposablePanel {
 
-    public static final Border BORDER_OBJECT_TYPE_CONTAINER_TOP = emptyTop(5);
+    public static final Border BORDER_OBJECT_TYPE_CONTAINER_TOP = empty(5, 0);
     public static final Border BORDER_OBJECT_TYPE_CONTENT = empty(3, 10);
 
     DefaultObjectTypeContainer(JComponent renderedComponent, String displayName, TooltipContent tooltipContent) {
