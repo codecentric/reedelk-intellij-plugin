fc301d718521a74c91d1782908c518102fb073bd
lorenzo <lorenzo@lorenzo.info.com>
2020-04-12 14:10:11 +0200

    code

--- src/main/java/com/reedelk/plugin/component/type/unknown/UnknownComponentPropertiesRenderer.java
+++ src/main/java/com/reedelk/plugin/component/type/unknown/UnknownComponentPropertiesRenderer.java
@@ -2,8 +2,10 @@ package com.reedelk.plugin.component.type.unknown;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.ui.components.JBLabel;
+import com.intellij.util.ui.JBEmptyBorder;
 import com.intellij.util.ui.JBUI;
 import com.reedelk.plugin.component.ComponentData;
+import com.reedelk.plugin.editor.properties.commons.ContainerFactory;
 import com.reedelk.plugin.editor.properties.commons.DisposablePanel;
 import com.reedelk.plugin.editor.properties.commons.FormBuilder;
 import com.reedelk.plugin.editor.properties.renderer.AbstractComponentPropertiesRenderer;
@@ -14,6 +16,7 @@ import java.awt.*;
 
 public class UnknownComponentPropertiesRenderer extends AbstractComponentPropertiesRenderer {
 
+    private static final JBEmptyBorder BORDER = JBUI.Borders.empty(5, 10, 0, 0);
     private static final int BORDER_TOP_BOTTOM = 5;
 
     public UnknownComponentPropertiesRenderer(FlowSnapshot snapshot, Module module) {
@@ -36,6 +39,7 @@ public class UnknownComponentPropertiesRenderer extends AbstractComponentPropert
                     .addLastField(label, propertiesPanel);
         });
 
-        return propertiesPanel;
+        propertiesPanel.setBorder(BORDER);
+        return ContainerFactory.pushTop(propertiesPanel);
     }
 }
