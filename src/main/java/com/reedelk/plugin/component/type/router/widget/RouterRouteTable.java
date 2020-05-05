b0a7926124fe0af9290b6d784c0a68060978a2cb
lorenzo <info@reedelk.com>
2020-05-05 08:12:19 +0200

    code

--- src/main/java/com/reedelk/plugin/component/type/router/widget/RouterRouteTable.java
+++ src/main/java/com/reedelk/plugin/component/type/router/widget/RouterRouteTable.java
@@ -2,13 +2,14 @@ package com.reedelk.plugin.component.type.router.widget;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.ui.JBColor;
-import com.intellij.util.ui.JBUI;
 import com.reedelk.plugin.commons.Colors;
 import com.reedelk.plugin.component.type.router.RouterConditionRoutePair;
 import com.reedelk.plugin.editor.properties.commons.DisposableTable;
 import com.reedelk.plugin.graph.FlowSnapshot;
 
 import java.util.List;
+
+import static com.intellij.util.ui.JBUI.Borders;
 
 public class RouterRouteTable extends DisposableTable {
 
@@ -17,13 +18,12 @@ public class RouterRouteTable extends DisposableTable {
                             List<RouterConditionRoutePair> conditionRoutePairList,
                             String componentPropertyPath) {
         super(module.getProject(),
-                new ConditionRouteTableModel(conditionRoutePairList, snapshot),
-                false);
+                new ConditionRouteTableModel(conditionRoutePairList, snapshot),false);
 
         ConditionRouteTableColumnModelFactory factory = new ConditionRouteTableColumnModelFactory(module, componentPropertyPath);
         factory.create(this);
         setOpaque(true);
         setBackground(Colors.PROPERTIES_EMPTY_SELECTION_BACKGROUND);
-        setBorder(JBUI.Borders.customLine(JBColor.LIGHT_GRAY, 0, 0, 1, 0));
+        setBorder(Borders.customLine(JBColor.LIGHT_GRAY, 0, 0, 1, 0));
     }
 }
