2ba12225cb61d4d1881c1044b996f4ad0284bf3a
lorenzo <info@reedelk.com>
2020-05-15 09:47:33 +0200

    code

--- src/main/java/com/reedelk/plugin/service/module/impl/component/completion/TypeDynamicUtils.java
+++ src/main/java/com/reedelk/plugin/service/module/impl/component/completion/TypeDynamicUtils.java
@@ -7,30 +7,13 @@ import com.reedelk.runtime.api.message.MessagePayload;
 import java.util.Collection;
 import java.util.List;
 
+import static com.reedelk.runtime.api.commons.StringUtils.isNotBlank;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
 public class TypeDynamicUtils {
 
     private TypeDynamicUtils() {
-    }
-
-    // Resolves the dynamic type from the output descriptor
-    public static List<String> from(Suggestion suggestion, ComponentOutputDescriptor descriptor) {
-        String suggestionType = suggestion.getReturnType();
-        if (MessageAttributes.class.getName().equals(suggestionType)) {
-            return descriptor != null && descriptor.getAttributes() != null ?
-                    singletonList(descriptor.getAttributes()) :
-                    singletonList(MessageAttributes.class.getName());
-
-        } else if (MessagePayload.class.getName().equals(suggestionType)) {
-            return descriptor != null && descriptor.getPayload() != null ?
-                    descriptor.getPayload() :
-                    singletonList(Object.class.getName());
-
-        } else {
-            return singletonList(Object.class.getName());
-        }
     }
 
     // We need to create an artificial suggestion for each dynamic type found.
@@ -40,7 +23,7 @@ public class TypeDynamicUtils {
     public static Collection<Suggestion> createDynamicSuggestion(ComponentOutputDescriptor descriptor,
                                                                  Suggestion suggestion,
                                                                  TypeAndTries typeAndTrieMap) {
-        return TypeDynamicUtils.from(suggestion, descriptor).stream()
+        return TypeDynamicUtils.resolve(suggestion, descriptor).stream()
                 .map(dynamicType -> Suggestion.create(suggestion.getType())
                         .returnTypeDisplayValue(TypeUtils.presentableTypeOf(suggestion, dynamicType, typeAndTrieMap))
                         .cursorOffset(suggestion.getCursorOffset())
@@ -55,5 +38,23 @@ public class TypeDynamicUtils {
     public static boolean is(Suggestion suggestion) {
         return MessagePayload.class.getName().equals(suggestion.getReturnType()) ||
                 MessageAttributes.class.getName().equals(suggestion.getReturnType());
+    }
+
+    // Resolves the dynamic type from the output descriptor
+    static List<String> resolve(Suggestion suggestion, ComponentOutputDescriptor descriptor) {
+        String suggestionType = suggestion.getReturnType();
+        if (MessageAttributes.class.getName().equals(suggestionType)) {
+            return descriptor != null && isNotBlank(descriptor.getAttributes()) ?
+                    singletonList(descriptor.getAttributes()) :
+                    singletonList(MessageAttributes.class.getName());
+
+        } else if (MessagePayload.class.getName().equals(suggestionType)) {
+            return descriptor != null && descriptor.getPayload() != null ?
+                    descriptor.getPayload() :
+                    singletonList(Object.class.getName());
+
+        } else {
+            throw new IllegalStateException("Resolve must be called only if the suggestion type is dynamic");
+        }
     }
 }
--- src/test/java/com/reedelk/plugin/service/module/impl/component/completion/TypeDynamicUtilsTest.java
+++ src/test/java/com/reedelk/plugin/service/module/impl/component/completion/TypeDynamicUtilsTest.java
@@ -1,13 +1,19 @@
 package com.reedelk.plugin.service.module.impl.component.completion;
 
+import com.reedelk.module.descriptor.model.component.ComponentOutputDescriptor;
 import com.reedelk.runtime.api.message.MessageAttributes;
 import com.reedelk.runtime.api.message.MessagePayload;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TypeDynamicUtilsTest {
 
+    // Is dynamic type tests
     @Test
     void shouldReturnTrueWhenSuggestionHasMessagePayloadReturnType() {
         // Given
@@ -71,5 +77,86 @@ class TypeDynamicUtilsTest {
 
         // Then
         assertThat(actual).isFalse();
+    }
+
+    // Resolve dynamic type tests
+    @Test
+    void shouldResolveMessageAttributesTypeFromDescriptor() {
+        // Given
+        String expectedType = "com.test.MyType";
+        ComponentOutputDescriptor outputDescriptor = new ComponentOutputDescriptor();
+        outputDescriptor.setAttributes(expectedType);
+
+        Suggestion attributes =
+                SuggestionTestUtils.createFunctionSuggestion("attributes()", MessageAttributes.class.getName());
+
+        // When
+        List<String> attributeTypes = TypeDynamicUtils.resolve(attributes, outputDescriptor);
+
+        // Then
+        assertThat(attributeTypes).containsOnly(expectedType);
+    }
+
+    @Test
+    void shouldResolveDefaultMessageAttributesTypeFromNullDescriptor() {
+        // Given
+        ComponentOutputDescriptor outputDescriptor = null;
+
+        Suggestion attributes =
+                SuggestionTestUtils.createFunctionSuggestion("attributes()", MessageAttributes.class.getName());
+
+        // When
+        List<String> attributeTypes = TypeDynamicUtils.resolve(attributes, outputDescriptor);
+
+        // Then
+        assertThat(attributeTypes).containsOnly(MessageAttributes.class.getName());
+    }
+
+    @Test
+    void shouldResolveMessagePayloadTypeFromDescriptor() {
+        // Given
+        List<String> expectedTypes = Arrays.asList(Integer.class.getName(), "com.test.MyType");
+        ComponentOutputDescriptor outputDescriptor = new ComponentOutputDescriptor();
+        outputDescriptor.setPayload(expectedTypes);
+
+        Suggestion payload =
+                SuggestionTestUtils.createFunctionSuggestion("payload()", MessagePayload.class.getName());
+
+        // When
+        List<String> types = TypeDynamicUtils.resolve(payload, outputDescriptor);
+
+        // Then
+        assertThat(types).containsExactlyInAnyOrder(Integer.class.getName(), "com.test.MyType");
+    }
+
+    @Test
+    void shouldResolveDefaultMessagePayloadTypeFromNullDescriptor() {
+        // Given
+        ComponentOutputDescriptor outputDescriptor = null;
+
+        Suggestion payload =
+                SuggestionTestUtils.createFunctionSuggestion("payload()", MessagePayload.class.getName());
+
+        // When
+        List<String> types = TypeDynamicUtils.resolve(payload, outputDescriptor);
+
+        // Then the default payload is Object type.
+        assertThat(types).containsOnly(Object.class.getName());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSuggestionTypeIsNotDynamic() {
+        // Given
+        ComponentOutputDescriptor outputDescriptor = new ComponentOutputDescriptor();
+        Suggestion method =
+                SuggestionTestUtils.createFunctionSuggestion("method()", "com.test.MyType");
+
+        // When
+        IllegalStateException thrown =
+                assertThrows(IllegalStateException.class, () -> TypeDynamicUtils.resolve(method, outputDescriptor));
+
+
+        // Then
+        assertThat(thrown).hasMessage("Resolve must be called only if the suggestion type is dynamic");
     }
 }
