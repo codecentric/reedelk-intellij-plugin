2f345065a34463cd2ae582eb03e6b79ec396bf30
lorenzo <info@reedelk.com>
2020-05-23 15:28:02 +0200

    code

--- src/main/java/com/reedelk/plugin/component/type/router/RouterComponentDiscovery.java
+++ src/main/java/com/reedelk/plugin/component/type/router/RouterComponentDiscovery.java
@@ -1,23 +1,18 @@
 package com.reedelk.plugin.component.type.router;
 
 import com.intellij.openapi.module.Module;
-import com.reedelk.plugin.component.type.generic.GenericComponentDiscovery;
 import com.reedelk.plugin.graph.node.GraphNode;
-import com.reedelk.plugin.graph.node.ScopedGraphNode;
 import com.reedelk.plugin.service.module.PlatformModuleService;
 import com.reedelk.plugin.service.module.impl.component.ComponentContext;
 import com.reedelk.plugin.service.module.impl.component.completion.TypeAndTries;
-import com.reedelk.plugin.service.module.impl.component.metadata.DiscoveryStrategy;
 import com.reedelk.plugin.service.module.impl.component.metadata.DiscoveryStrategyFactory;
+import com.reedelk.plugin.service.module.impl.component.metadata.DiscoveryStrategyOneOfAware;
 import com.reedelk.plugin.service.module.impl.component.metadata.PreviousComponentOutput;
-import com.reedelk.plugin.service.module.impl.component.metadata.PreviousComponentOutputOneOf;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-public class RouterComponentDiscovery extends GenericComponentDiscovery {
+public class RouterComponentDiscovery extends DiscoveryStrategyOneOfAware {
 
     public RouterComponentDiscovery(@NotNull Module module,
                                     @NotNull PlatformModuleService moduleService,
@@ -31,35 +26,6 @@ public class RouterComponentDiscovery extends GenericComponentDiscovery {
         // This is because the router forwards the input message to the outbound component matching
         // the given condition.
         return discover(context, currentNode);
-    }
-
-    @Override
-    public Optional<PreviousComponentOutput> compute(ComponentContext context, ScopedGraphNode scopedGraphNode) {
-
-        // The output is one of the branches evaluated true
-        List<PreviousComponentOutput> outputs = new ArrayList<>();
-        List<GraphNode> lastNodesOfScope = context.listLastNodesOfScope(scopedGraphNode);
-
-        for (GraphNode lastNodeOfScope : lastNodesOfScope) {
-
-            // Extract a method for each node
-            Optional<ScopedGraphNode> theScopeOfLastNode = context.findScopeOf(lastNodeOfScope);
-            if (theScopeOfLastNode.isPresent() && theScopeOfLastNode.get() != scopedGraphNode) {
-                // In a different scope and there is no JOIN in the middle need to lookup for the scope
-                String innerScopeFullyQualifiedName = theScopeOfLastNode.get().componentData().getFullyQualifiedName();
-                DiscoveryStrategy strategy =
-                        DiscoveryStrategyFactory.get(module, moduleService, typeAndAndTries, innerScopeFullyQualifiedName);
-                strategy.compute(context, theScopeOfLastNode.get()).ifPresent(outputs::add);
-
-            } else {
-                String nodeFullyQualifiedName = lastNodeOfScope.componentData().getFullyQualifiedName();
-                DiscoveryStrategy strategy =
-                        DiscoveryStrategyFactory.get(module, moduleService, typeAndAndTries, nodeFullyQualifiedName);
-                strategy.compute(context, lastNodeOfScope).ifPresent(outputs::add);
-            }
-        }
-
-        return Optional.of(new PreviousComponentOutputOneOf(outputs));
     }
 
     Optional<PreviousComponentOutput> discover(ComponentContext context, GraphNode target) {
--- src/main/java/com/reedelk/plugin/component/type/trycatch/TryCatchComponentDiscovery.java
+++ src/main/java/com/reedelk/plugin/component/type/trycatch/TryCatchComponentDiscovery.java
@@ -2,33 +2,28 @@ package com.reedelk.plugin.component.type.trycatch;
 
 import com.intellij.openapi.module.Module;
 import com.reedelk.plugin.graph.node.GraphNode;
-import com.reedelk.plugin.graph.node.ScopedGraphNode;
 import com.reedelk.plugin.service.module.PlatformModuleService;
 import com.reedelk.plugin.service.module.impl.component.ComponentContext;
 import com.reedelk.plugin.service.module.impl.component.completion.TypeAndTries;
-import com.reedelk.plugin.service.module.impl.component.metadata.*;
+import com.reedelk.plugin.service.module.impl.component.metadata.DiscoveryStrategyFactory;
+import com.reedelk.plugin.service.module.impl.component.metadata.DiscoveryStrategyOneOfAware;
+import com.reedelk.plugin.service.module.impl.component.metadata.PreviousComponentOutput;
+import com.reedelk.plugin.service.module.impl.component.metadata.PreviousComponentOutputDefault;
 import com.reedelk.runtime.api.commons.StringUtils;
 import com.reedelk.runtime.api.message.MessageAttributes;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static java.util.Collections.singletonList;
 
-public class TryCatchComponentDiscovery implements DiscoveryStrategy {
-
-    protected final PlatformModuleService moduleService;
-    protected final TypeAndTries typeAndAndTries;
-    protected final Module module;
+public class TryCatchComponentDiscovery extends DiscoveryStrategyOneOfAware {
 
     public TryCatchComponentDiscovery(@NotNull Module module,
                                       @NotNull PlatformModuleService moduleService,
                                       @NotNull TypeAndTries typeAndAndTries) {
-        this.typeAndAndTries = typeAndAndTries;
-        this.moduleService = moduleService;
-        this.module = module;
+        super(module, moduleService, typeAndAndTries);
     }
 
     @Override
@@ -45,35 +40,6 @@ public class TryCatchComponentDiscovery implements DiscoveryStrategy {
                     StringUtils.EMPTY);
             return Optional.of(descriptor);
         }
-    }
-
-    @Override
-    public Optional<PreviousComponentOutput> compute(ComponentContext context, ScopedGraphNode scopedGraphNode) {
-
-        // The output is either from the try or from the catch
-        List<PreviousComponentOutput> outputs = new ArrayList<>();
-        List<GraphNode> lastNodesOfScope = context.listLastNodesOfScope(scopedGraphNode);
-
-        for (GraphNode lastNodeOfScope : lastNodesOfScope) {
-
-            // Extract a method for each node
-            Optional<ScopedGraphNode> theScopeOfLastNode = context.findScopeOf(lastNodeOfScope);
-            if (theScopeOfLastNode.isPresent() && theScopeOfLastNode.get() != scopedGraphNode) {
-                // In a different scope and there is no JOIN in the middle need to lookup for the scope
-                String innerScopeFullyQualifiedName = theScopeOfLastNode.get().componentData().getFullyQualifiedName();
-                DiscoveryStrategy strategy =
-                        DiscoveryStrategyFactory.get(module, moduleService, typeAndAndTries, innerScopeFullyQualifiedName);
-                strategy.compute(context, theScopeOfLastNode.get()).ifPresent(outputs::add);
-
-            } else {
-                String nodeFullyQualifiedName = lastNodeOfScope.componentData().getFullyQualifiedName();
-                DiscoveryStrategy strategy =
-                        DiscoveryStrategyFactory.get(module, moduleService, typeAndAndTries, nodeFullyQualifiedName);
-                strategy.compute(context, lastNodeOfScope).ifPresent(outputs::add);
-            }
-        }
-
-        return Optional.of(new PreviousComponentOutputOneOf(outputs));
     }
 
     Optional<PreviousComponentOutput> discover(ComponentContext context, GraphNode target) {
