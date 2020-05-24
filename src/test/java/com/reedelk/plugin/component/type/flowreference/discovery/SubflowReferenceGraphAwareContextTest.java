0a7374566fe2c0f00fbc418fa2509f546f2137a2
lorenzo <info@reedelk.com>
2020-05-24 11:04:42 +0200

    code

--- src/test/java/com/reedelk/plugin/component/type/flowreference/discovery/SubflowReferenceGraphAwareContextTest.java
+++ src/test/java/com/reedelk/plugin/component/type/flowreference/discovery/SubflowReferenceGraphAwareContextTest.java
@@ -3,10 +3,12 @@ package com.reedelk.plugin.component.type.flowreference.discovery;
 import com.reedelk.plugin.AbstractGraphTest;
 import com.reedelk.plugin.graph.FlowGraph;
 import com.reedelk.plugin.graph.node.GraphNode;
+import com.reedelk.plugin.graph.node.ScopedGraphNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,7 +24,12 @@ class SubflowReferenceGraphAwareContextTest extends AbstractGraphTest {
         subflowGraph.root(root);
         subflowGraph.add(root, componentNode1);
         subflowGraph.add(componentNode1, componentNode2);
-        subflowGraph.add(componentNode2, componentNode3);
+        subflowGraph.add(componentNode2, forkNode1);
+        subflowGraph.add(forkNode1, componentNode3);
+        subflowGraph.add(forkNode1, componentNode4);
+
+        forkNode1.addToScope(componentNode3);
+        forkNode1.addToScope(componentNode4);
 
         context = new SubflowReferenceGraphAwareContext(subflowGraph, flowReferenceNode1);
     }
@@ -33,15 +40,24 @@ class SubflowReferenceGraphAwareContextTest extends AbstractGraphTest {
         List<GraphNode> predecessors = context.predecessors(flowReferenceNode1);
 
         // Then
-        assertThat(predecessors).contains(componentNode3);
+        assertThat(predecessors).containsExactlyInAnyOrder(componentNode3, componentNode4);
     }
 
     @Test
     void shouldReturnCorrectPredecessorOfSubflowGraphNode() {
         // When
-        List<GraphNode> predecessors = context.predecessors(componentNode3);
+        List<GraphNode> predecessors = context.predecessors(componentNode2);
 
         // Then
-        assertThat(predecessors).contains(componentNode2);
+        assertThat(predecessors).contains(componentNode1);
+    }
+
+    @Test
+    void shouldReturnCorrectJoiningScopeWhenTargetIsFlowReference() {
+        // When
+        Optional<ScopedGraphNode> scopedGraphNode = context.joiningScopeOf(flowReferenceNode1);
+
+        // Then
+        assertThat(scopedGraphNode).contains(forkNode1);
     }
 }
