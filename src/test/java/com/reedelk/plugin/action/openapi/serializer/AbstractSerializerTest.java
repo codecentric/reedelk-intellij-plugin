c157e008612323e5e594a443f8b7ec55ee066664
lorenzo <info@reedelk.com>
2020-08-18 09:53:23 +0200

    feature: working on OpenAPI import tool.

--- src/test/java/com/reedelk/plugin/action/openapi/serializer/AbstractSerializerTest.java
+++ src/test/java/com/reedelk/plugin/action/openapi/serializer/AbstractSerializerTest.java
@@ -1,9 +1,9 @@
 package com.reedelk.plugin.action.openapi.serializer;
 
+import com.reedelk.openapi.commons.DataFormats;
 import com.reedelk.openapi.commons.NavigationPath;
 import com.reedelk.openapi.v3.SerializerContext;
 import com.reedelk.openapi.v3.serializer.Serializers;
-import com.reedelk.plugin.action.openapi.DataFormats;
 import com.reedelk.plugin.action.openapi.OpenApiImporterContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
